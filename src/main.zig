bbc737fc338b7e1f34a43baacdb29558e8539d86
Jason Phan <jason@ket.so>
2021-04-07 13:31:15 -0500

    Update public declarations to use derive dir

--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,2 @@
-pub const Serialize = @import("interfaces/serialize.zig").Serialize;
-pub const Deserialize = @import("interfaces/deserialize.zig").Deserialize;
+pub const Serialize = @import("derive/serialize.zig").Serialize;
+pub const Deserialize = @import("derive/deserialize.zig").Deserialize;
