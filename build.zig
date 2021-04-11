bf351ebce12eb0818c21c785ca1a87468b8a66b5
Jason Phan <jason@ket.so>
2021-04-11 10:55:53 -0500

    Update build process

--- build.zig
+++ build.zig
@@ -1,14 +1,52 @@
-const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
+const std = @import("std");
 
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("getty", "src/main.zig");
+const Builder = std.build.Builder;
+const Pkg = std.build.Pkg;
+
+pub fn build(builder: *Builder) void {
+    // Standard release options (Debug, ReleaseSafe, ReleaseSmall, ReleaseFast)
+    const mode = builder.standardReleaseOptions();
+    //const modes = [_]std.builtin.Mode{ .Debug, .ReleaseSafe, .ReleaseFast, .ReleaseSmall };
+    const modes = [_]std.builtin.Mode{.Debug};
+
+    // Standard target options (any target is allowed; default is native)
+    const target = builder.standardTargetOptions(.{});
+
+    // Tests
+    const test_step = builder.step("test", "Run library tests.");
+    const test_files = [_][]const u8{
+        "src/serialize.zig",
+        "src/deserialize.zig",
+        "src/derive/serialize.zig",
+        "src/derive/deserialize.zig",
+    };
+
+    inline for (&modes) |m| {
+        const mode_str = switch (m) {
+            .Debug => "DEBUG",
+            .ReleaseSafe => "RELEASE-SAFE",
+            .ReleaseFast => "RELEASE-FAST",
+            .ReleaseSmall => "RELEASE-SMALL",
+        };
+
+        const step = builder.step("(" ++ mode_str ++ ")", "Run tests in " ++ mode_str ++ " mode.");
+        test_step.dependOn(step);
+
+        inline for (&test_files) |f| {
+            const tests = builder.addTest(f);
+
+            tests.addPackagePath("getty", "src/main.zig");
+            tests.setBuildMode(m);
+            tests.setTarget(target);
+            //tests.setNamePrefix(mode_str ++ " ");
+
+            step.dependOn(&tests.step);
+        }
+    }
+
+    // Library
+    const lib = builder.addStaticLibrary("getty", "src/main.zig");
     lib.setBuildMode(mode);
     lib.install();
-
-    var main_tests = b.addTest("src/tests.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
 }
--- src/tests.zig
+++
@@ -1,6 +0,0 @@
-usingnamespace @import("attribute.zig");
-usingnamespace @import("deserialize.zig");
-usingnamespace @import("serialize.zig");
-
-usingnamespace @import("derive/deserialize.zig");
-usingnamespace @import("derive/serialize.zig");
