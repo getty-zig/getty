6c80912913fa0ede0fba6b5c0fc502cf83a72bcc
Jason Phan <jason@ket.so>
2021-04-16 13:28:46 -0500

    Revert back to fat pointer interfaces
    The other way didn't make sense as the `serialize` method takes a
    Serializer, but Serializer is a function...

--- src/deserialize.zig
+++ src/deserialize.zig
@@ -1,30 +1,73 @@
 const std = @import("std");
 
-pub fn Deserialize(comptime Context: type, deserialize_fn: fn (context: Context, deserializer: Deserializer) !void) type {
-    return struct {
-        const Self = @This();
+const Deserialize = struct {
+    const Address = usize;
+    const Error = error{};
+    const VTable = struct { deserialize: fn (Address, Deserializer) Error!void };
 
-        context: Context,
+    object: Address,
+    vtable: *const VTable,
 
-        pub fn deserialize(self: Self, deserializer: Deserializer) !void {
-            return try deserialize_fn(self.context, deserializer);
-        }
-    };
-}
+    fn init(obj: anytype) @This() {
+        const Pointer = @TypeOf(obj);
 
-pub fn Deserializer(
-    comptime Context: type,
-) type {
-    return struct {
-        const Self = @This();
+        const deserialize_fn = struct {
+            fn deserialize(address: Address, deserializer: Deserializer) Error!void {
+                @call(.{ .modifier = .always_inline }, std.meta.Child(Pointer).deserialize, .{ @intToPtr(Pointer, address), deserializer });
+            }
+        }.deserialize;
 
-        context: Context,
-    };
-}
+        return .{
+            .object = @ptrToInt(obj),
+            .vtable = &comptime VTable{ .deserialize = deserialize_fn },
+        };
+    }
+
+    fn deserialize(self: @This(), deserializer: Deserializer) Error!void {
+        try self.vtable.deserialize(self.object, deserializer);
+    }
+};
+
+pub const Deserializer = struct {
+    const Address = usize;
+    const VTable = struct {};
+
+    vtable: *const VTable,
+    object: Address,
+
+    fn init(obj: anytype) @This() {
+        const Pointer = @TypeOf(obj);
+
+        return .{
+            .object = @ptrToInt(obj),
+            .vtable = &comptime VTable{},
+        };
+    }
+};
+
+const TestPoint = struct {
+    x: i32,
+    y: i32,
+
+    fn deserialize(self: *@This(), deserializer: Deserializer) void {
+        std.log.warn("Deserialize", .{});
+    }
+};
+
+const TestDeserializer = struct {
+    v: bool,
+};
 
 test "Deserialize - init" {
-    const Point = struct {
-        x: i32,
-        y: i32,
-    };
+    var p = TestPoint{ .x = 1, .y = 2 };
+    var s = TestDeserializer{ .v = true };
+
+    var deserialize = Deserialize.init(&p);
+    var deserializer = Deserializer.init(&s);
+
+    try deserialize.deserialize(deserializer);
+}
+
+comptime {
+    std.testing.refAllDecls(@This());
 }
--- src/serialize.zig
+++ src/serialize.zig
@@ -1,37 +1,90 @@
 const std = @import("std");
 
-pub fn Serialize(comptime Context: type, serialize_fn: fn (context: Context, serializer: Serializer) !void) type {
-    return struct {
-        const Self = @This();
+pub const Serialize = struct {
+    const Address = usize;
+    const Error = error{};
+    const VTable = struct { serialize: fn (Address, Serializer) Error!void };
 
-        context: Context,
+    object: Address,
+    vtable: *const VTable,
 
-        pub fn serialize(self: Self, serializer: Serializer) !void {
-            return try serialize_fn(self.context, serializer);
-        }
+    fn init(obj: anytype) @This() {
+        const Pointer = @TypeOf(obj);
+
+        const serialize_fn = struct {
+            fn serialize(address: Address, serializer: Serializer) Error!void {
+                @call(.{ .modifier = .always_inline }, std.meta.Child(Pointer).serialize, .{ @intToPtr(Pointer, address), serializer });
+            }
+        }.serialize;
+
+        return .{
+            .object = @ptrToInt(obj),
+            .vtable = &comptime VTable{ .serialize = serialize_fn },
+        };
+    }
+
+    fn serialize(self: @This(), serializer: Serializer) Error!void {
+        try self.vtable.serialize(self.object, serializer);
+    }
+};
+
+pub const Serializer = struct {
+    const Address = usize;
+    const Error = error{};
+    const VTable = struct {
+        serialize_bool: fn (Address, bool) Error!void
     };
-}
 
-pub fn Serializer(
-    comptime Context: type,
-    serialize_bool_fn: fn (context: Context, v: bool) !void,
-) type {
-    return struct {
-        const Self = @This();
+    object: Address,
+    vtable: *const VTable,
 
-        context: Context,
+    fn init(obj: anytype) @This() {
+        const Pointer = @TypeOf(obj);
 
-        pub fn serialize_bool(self: Self, v: bool) !void {
-            return try serialize_bool_fn(self.context, v);
-        }
-    };
-}
+        const serialize_bool_fn = struct {
+            fn serialize_bool(address: Address, v: bool) Error!void {
+                @call(.{ .modifier = .always_inline }, std.meta.Child(Pointer).serialize_bool, .{ @intToPtr(Pointer, address), v });
+            }
+        }.serialize_bool;
+
+        return .{
+            .object = @ptrToInt(obj),
+            .vtable = &comptime VTable{ .serialize_bool = serialize_bool_fn },
+        };
+    }
+
+    pub fn serialize_bool(self: @This(), v: bool) Error!void {
+        try self.vtable.serialize_bool(self.object, v);
+    }
+};
+
+const TestPoint = struct {
+    x: i32,
+    y: i32,
+
+    fn serialize(self: *@This(), serializer: Serializer) void {
+        std.log.warn("Serialize", .{});
+    }
+};
+
+const TestSerializer = struct {
+    // FIXME: Serializer with no state gives a poitner of size 0, making Serializer.init() fail.
+    v: bool,
+
+    fn serialize_bool(self: *@This(), v: bool) void {
+        std.log.warn("Serializer", .{});
+    }
+};
 
 test "Serialize - init" {
-    const Point = struct {
-        x: i32,
-        y: i32,
-    };
+    var p = TestPoint{ .x = 1, .y = 2 };
+    var s = TestSerializer{ .v = true };
+
+    var serialize = Serialize.init(&p);
+    var serializer = Serializer.init(&s);
+
+    try serialize.serialize(serializer);
+    try serializer.serialize_bool(true);
 }
 
 comptime {
