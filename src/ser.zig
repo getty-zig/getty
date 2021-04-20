e3078bf59f8cae1f8047fd54e85e69411429ec1e
Jason Phan <jason@ket.so>
2021-04-20 14:08:18 -0500

    Update TestSerializer to be a function interface

--- src/ser.zig
+++ src/ser.zig
@@ -140,25 +140,20 @@ pub fn Serializer(
 
 test "Serialize - init" {
     var p = TestPoint{ .x = 1, .y = 2 };
-    //var s = TestSerializer.init(std.testing.allocator);
-    //defer s.deinit();
+    var s = TestSerializer.init(std.testing.allocator);
+    defer s.deinit();
 
     var ser = p.ser();
-    _ = try ser.serialize(void, error{}, .{});
-    //var ser = &(@TypeOf(p).ser);
-    //var serializer = &(@TypeOf(s).serializer);
-    //try ser.serialize(serializer.*);
-    //try serializer.serialize_bool(true);
+    var serializer = s.serializer();
+    try ser.serialize(void, error{}, .{});
+    try serializer.serialize_bool(true);
 }
 
 const TestPoint = struct {
     x: i32,
     y: i32,
 
-    const Ser = Serialize(
-        *@This(),
-        serialize,
-    );
+    const Ser = Serialize(*@This(), serialize);
 
     fn ser(self: *@This()) Ser {
         return .{ .context = self };
@@ -169,90 +164,100 @@ const TestPoint = struct {
     }
 };
 
-//const TestSerializer = struct {
-//output: std.ArrayList(u8),
+const TestSerializer = struct {
+    output: std.ArrayList(u8),
 
-//fn init(allocator: *std.mem.Allocator) @This() {
-//return .{ .output = std.ArrayList(u8).init(allocator) };
-//}
+    fn init(allocator: *std.mem.Allocator) @This() {
+        return .{ .output = std.ArrayList(u8).init(allocator) };
+    }
 
-//fn deinit(self: @This()) void {
-//self.output.deinit();
-//}
+    fn deinit(self: @This()) void {
+        self.output.deinit();
+    }
 
-//const serializer = Serializer{
-//.bool_fn = serialize_bool,
-//.i8_fn = serialize_i8,
-//.i16_fn = serialize_i16,
-//.i32_fn = serialize_i32,
-//.i64_fn = serialize_i64,
-//.i128_fn = serialize_i128,
-//.u8_fn = serialize_u8,
-//.u16_fn = serialize_u16,
-//.u32_fn = serialize_u32,
-//.u64_fn = serialize_u64,
-//.u128_fn = serialize_u128,
-//.f16_fn = serialize_f16,
-//.f32_fn = serialize_f32,
-//.f64_fn = serialize_f64,
-//};
+    const Ok = void;
+    const Error = error{};
 
-//fn serialize_bool(self: *const Serializer, v: bool) Error!void {
-//std.log.warn("TestSerializer.serialize_bool", .{});
-//}
+    const S = Serializer(
+        *@This(),
+        Ok,
+        Error,
+        serialize_bool,
+        serialize_i8,
+        serialize_i16,
+        serialize_i32,
+        serialize_i64,
+        serialize_i128,
+        serialize_u8,
+        serialize_u16,
+        serialize_u32,
+        serialize_u64,
+        serialize_u128,
+        serialize_f16,
+        serialize_f32,
+        serialize_f64,
+    );
 
-//fn serialize_i8(self: *const Serializer, v: i8) Error!void {
-//std.log.warn("TestSerializer.serialize_i8", .{});
-//}
+    fn serializer(self: *@This()) S {
+        return .{ .context = self };
+    }
 
-//fn serialize_i16(self: *const Serializer, v: i16) Error!void {
-//std.log.warn("TestSerializer.serialize_i16", .{});
-//}
+    fn serialize_bool(self: *@This(), v: bool) Error!Ok {
+        std.log.warn("TestSerializer.serialize_bool", .{});
+    }
 
-//fn serialize_i32(self: *const Serializer, v: i32) Error!void {
-//std.log.warn("TestSerializer.serialize_i32", .{});
-//}
+    fn serialize_i8(self: *@This(), v: i8) Error!Ok {
+        std.log.warn("TestSerializer.serialize_i8", .{});
+    }
 
-//fn serialize_i64(self: *const Serializer, v: i64) Error!void {
-//std.log.warn("TestSerializer.serialize_i64", .{});
-//}
+    fn serialize_i16(self: *@This(), v: i16) Error!Ok {
+        std.log.warn("TestSerializer.serialize_i16", .{});
+    }
 
-//fn serialize_i128(self: *const Serializer, v: i128) Error!void {
-//std.log.warn("TestSerializer.serialize_i128", .{});
-//}
+    fn serialize_i32(self: *@This(), v: i32) Error!Ok {
+        std.log.warn("TestSerializer.serialize_i32", .{});
+    }
 
-//fn serialize_u8(self: *const Serializer, v: u8) Error!void {
-//std.log.warn("TestSerializer.serialize_u8", .{});
-//}
+    fn serialize_i64(self: *@This(), v: i64) Error!Ok {
+        std.log.warn("TestSerializer.serialize_i64", .{});
+    }
 
-//fn serialize_u16(self: *const Serializer, v: u16) Error!void {
-//std.log.warn("TestSerializer.serialize_u16", .{});
-//}
+    fn serialize_i128(self: *@This(), v: i128) Error!Ok {
+        std.log.warn("TestSerializer.serialize_i128", .{});
+    }
 
-//fn serialize_u32(self: *const Serializer, v: u32) Error!void {
-//std.log.warn("TestSerializer.serialize_u32", .{});
-//}
+    fn serialize_u8(self: *@This(), v: u8) Error!Ok {
+        std.log.warn("TestSerializer.serialize_u8", .{});
+    }
 
-//fn serialize_u64(self: *const Serializer, v: u64) Error!void {
-//std.log.warn("TestSerializer.serialize_u64", .{});
-//}
+    fn serialize_u16(self: *@This(), v: u16) Error!Ok {
+        std.log.warn("TestSerializer.serialize_u16", .{});
+    }
 
-//fn serialize_u128(self: *const Serializer, v: u128) Error!void {
-//std.log.warn("TestSerializer.serialize_u128", .{});
-//}
+    fn serialize_u32(self: *@This(), v: u32) Error!Ok {
+        std.log.warn("TestSerializer.serialize_u32", .{});
+    }
 
-//fn serialize_f16(self: *const Serializer, v: f16) Error!void {
-//std.log.warn("TestSerializer.serialize_f16", .{});
-//}
+    fn serialize_u64(self: *@This(), v: u64) Error!Ok {
+        std.log.warn("TestSerializer.serialize_u64", .{});
+    }
 
-//fn serialize_f32(self: *const Serializer, v: f32) Error!void {
-//std.log.warn("TestSerializer.serialize_f32", .{});
-//}
+    fn serialize_u128(self: *@This(), v: u128) Error!Ok {
+        std.log.warn("TestSerializer.serialize_u128", .{});
+    }
 
-//fn serialize_f64(self: *const Serializer, v: f64) Error!void {
-//std.log.warn("TestSerializer.serialize_f64", .{});
-//}
-//};
+    fn serialize_f16(self: *@This(), v: f16) Error!Ok {
+        std.log.warn("TestSerializer.serialize_f16", .{});
+    }
+
+    fn serialize_f32(self: *@This(), v: f32) Error!Ok {
+        std.log.warn("TestSerializer.serialize_f32", .{});
+    }
+
+    fn serialize_f64(self: *@This(), v: f64) Error!Ok {
+        std.log.warn("TestSerializer.serialize_f64", .{});
+    }
+};
 
 comptime {
     std.testing.refAllDecls(@This());
