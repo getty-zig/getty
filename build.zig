67253bf8d0927977ad4fc7bf319403aad8da0372
Jason Phan <jason@ket.so>
2021-05-21 11:48:59 -0500

    build: builder -> b

--- build.zig
+++ build.zig
@@ -2,29 +2,27 @@ const std = @import("std");
 
 const Builder = std.build.Builder;
 
-//const MODES = [_]std.builtin.Mode{ .Debug, .ReleaseSafe, .ReleaseFast, .ReleaseSmall };
 const MODES = [_]std.builtin.Mode{.Debug};
 
 const PACKAGE_NAME = "getty";
 const PACKAGE_PATH = "src/lib.zig";
 
 const TEST_FILES = [_][]const u8{
-    //"src/de.zig",
     "src/attributes.zig",
     "src/ser.zig",
     "src/json.zig",
 };
 
-pub fn build(builder: *Builder) void {
-    const mode = builder.standardReleaseOptions();
-    const target = builder.standardTargetOptions(.{});
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
-    tests(builder, target);
-    library(builder, mode);
+    tests(b, target);
+    library(b, mode);
 }
 
-fn tests(builder: *Builder, target: std.zig.CrossTarget) void {
-    const test_step = builder.step("test", "Run library tests.");
+fn tests(b: *Builder, target: std.zig.CrossTarget) void {
+    const test_step = b.step("test", "Run library tests.");
 
     inline for (&MODES) |m| {
         const mode = switch (m) {
@@ -34,24 +32,23 @@ fn tests(builder: *Builder, target: std.zig.CrossTarget) void {
             .ReleaseSmall => "RELEASE-SMALL",
         };
 
-        const step = builder.step("(" ++ mode ++ ")", "Run tests in " ++ mode ++ " mode.");
+        const step = b.step("(" ++ mode ++ ")", "Run tests in " ++ mode ++ " mode.");
         test_step.dependOn(step);
 
         inline for (&TEST_FILES) |f| {
-            const t = builder.addTest(f);
+            const t = b.addTest(f);
 
             t.addPackagePath(PACKAGE_NAME, PACKAGE_PATH);
             t.setBuildMode(m);
             t.setTarget(target);
-            //t.setNamePrefix(mode ++ " ");
 
             step.dependOn(&t.step);
         }
     }
 }
 
-fn library(builder: *Builder, mode: std.builtin.Mode) void {
-    const lib = builder.addStaticLibrary(PACKAGE_NAME, PACKAGE_PATH);
+fn library(b: *Builder, mode: std.builtin.Mode) void {
+    const lib = b.addStaticLibrary(PACKAGE_NAME, PACKAGE_PATH);
     lib.setBuildMode(mode);
     lib.install();
 }
