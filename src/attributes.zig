f2a70c1ca020c873255bf7c951beb50b230946f4
Jason Phan <jason@ket.so>
2021-05-22 00:36:37 -0500

    attributes: Make attributes more ergonomic (#4)
    Closes #4.

--- src/attributes.zig
+++ src/attributes.zig
@@ -2,9 +2,15 @@ const de = @import("de.zig");
 const ser = @import("ser.zig");
 const std = @import("std");
 
-pub const Mode = enum {
-    ser,
-    de,
+pub const Case = enum {
+    lower,
+    upper,
+    pascal,
+    camel,
+    snake,
+    screaming_snake,
+    kebab,
+    screaming_kebab,
 };
 
 /// Returns an attribute map type.
@@ -73,7 +79,7 @@ pub const Mode = enum {
 ///     y: i32,
 /// };
 /// ```
-pub fn Attributes(comptime T: type, comptime mode: Mode, attributes: _Attributes(T, mode)) type {
+pub fn Attributes(comptime T: type, attributes: _Attributes(T)) type {
     return struct {
         pub const _attributes = attributes;
     };
@@ -82,77 +88,194 @@ pub fn Attributes(comptime T: type, comptime mode: Mode, attributes: _Attributes
 /// Returns an attribute map type.
 ///
 /// See `Attributes` for more information.
-fn _Attributes(comptime T: type, comptime mode: Mode) type {
-    const Container = switch (mode) {
-        .ser => struct {
-            //bound: ,
-            //content: ,
-            into: []const u8 = "",
-            rename: []const u8 = @typeName(T),
-            rename_all: []const u8 = "",
-            //remote: ,
-            //tag: ,
-            transparent: bool = false,
-            //untagged: ,
-        },
-        .de => struct {
-            //bound: ,
-            //content: ,
-            //default: ,
-            deny_unknown_fields: bool = false,
-            //from: ,
-            into: []const u8 = "",
-            rename: []const u8 = @typeName(T),
-            rename_all: []const u8 = "",
-            //remote: ,
-            //tag: ,
-            transparent: bool = false,
-            //try_from:,
-            //untagged: ,
-        },
+fn _Attributes(comptime T: type) type {
+    const Container = struct {
+        //content: ?[]const u8 = null,
+
+        // When deserializing, any missing fields should be filled in from the
+        // field's default value.
+        default: bool = false,
+
+        // When deserializing, any missing fields should be filled in from the
+        // object returned by the given function.
+        //
+        // Overrides `default`.
+        default_path: ?[]const u8 = null,
+
+        // Always error during deserialization when encountering unknown fields.
+        //
+        // This attribute isn't compatible with `flatten`.
+        deny_unknown_fields: bool = false,
+
+        // Deserialize this type by deserializing into the given type, then
+        // converting.
+        from: ?[]const u8 = null,
+
+        // Serialize this type by converting it into the specified type and
+        // serializing that.
+        into: ?[]const u8 = null,
+
+        // Serialize and deserialize this struct with the given name instead of
+        // its type name.
+        rename: []const u8 = @typeName(T),
+
+        // Deserialize this struct with the given name instead of its type
+        // name.
+        //
+        // Overrides `rename`.
+        rename_de: []const u8 = @typeName(T),
+
+        // Serialize this struct with the given name instead of its type name.
+        //
+        // Overrides `rename`.
+        rename_ser: []const u8 = @typeName(T),
+
+        // Rename all the fields of this struct according to the given case
+        // convention.
+        rename_all: ?Case = null,
+
+        // Rename all the fields of this struct during deserialization
+        // according to the given case convention.
+        //
+        // Overrides `rename_all`.
+        rename_all_de: ?Case = null,
+
+        // Rename all the fields of this struct during serialization according
+        // to the given case convention.
+        //
+        // Overrides `rename_all`.
+        rename_all_ser: ?Case = null,
+
+        // Use the internally tagged enum representation for this enum, with
+        // the given tag.
+        //tag: ?[]const u8 = null,
+
+        // Deserialize this type by deserializing into the given type, then
+        // converting fallibly.
+        try_from: ?[]const u8 = null,
+
+        // Use the untagged enum representation for this enum.
+        untagged: bool = false,
     };
 
-    const Inner = switch (mode) {
-        .ser => switch (@typeInfo(T)) {
-            .Struct => struct {
-                //bound: ,
-                //flatten: ,
-                //getter: ,
-                rename: []const u8 = @typeName(T),
-                skip: bool = false,
-                //skip_serializing_if: ,
-                with: []const u8 = "",
-            },
-            .Enum => struct {
-                //bound: ,
-                rename: []const u8 = "",
-                rename_all: []const u8 = "",
-                skip: bool = false,
-                with: []const u8 = "",
-            },
-            else => unreachable,
+    const Inner = switch (@typeInfo(T)) {
+        .Struct => struct {
+            // Deserialize this field from the given names *or* its type
+            // name.
+            alias: ?[][]const u8 = null,
+
+            // If the value is not present when deserializing, use the
+            // field's default value.
+            default: bool = false,
+
+            // If the value is not present when deserializing, call a
+            // function to get a default value.
+            //
+            // Overrides `default`.
+            default_path: ?[]const u8 = null,
+
+            // Deserialize this field using a function that is different
+            // from the normal deserialization implementation.
+            deserialize_with: ?[]const u8 = null,
+
+            // Flatten the contents of this field into the container it is
+            // defined in.
+            //
+            // This attribute is not compatible with the
+            // deny_unknown_fields attribute.
+            flatten: bool = false,
+
+            // Serialize and deserialize this field with the given name
+            // instead of its type name.
+            rename: ?[]const u8 = null,
+
+            // Deserialize this field with the given name instead of its
+            // type name.
+            //
+            // Overrides `rename`.
+            rename_de: ?[]const u8 = null,
+
+            // Serialize this field with the given name instead of its
+            // type name.
+            //
+            // Overrides `rename`.
+            rename_ser: ?[]const u8 = null,
+
+            // Skip this field during serialization and deserialziation.
+            //
+            // During deserialization, the field's default value will be
+            // used.
+            skip: bool = false,
+
+            // Skip this field during deserialization.
+            //
+            // The field's default value will be used.
+            //
+            // Overrides `skip`.
+            skip_de: bool = false,
+
+            // Skip this field during serialization.
+            //
+            // Overrides `skip`.
+            skip_ser: bool = false,
+
+            // Call a function to determine whether to skip serializing
+            // this field.
+            skip_ser_if: ?[]const u8 = null,
+
+            // Serialize this field using a function that is different from
+            // the normal serialization implementation.
+            serialize_with: ?[]const u8 = null,
+
+            // Combination of serialize_with and deserialize_with.
+            with: ?[]const u8 = null,
         },
-        .de => switch (@typeInfo(T)) {
-            .Struct => struct {
-                alias: []const u8 = "",
-                //bound: ,
-                //default: ,
-                //flatten: ,
-                rename: []const u8 = @typeName(T),
-                skip: bool = false,
-                with: []const u8 = "",
-            },
-            .Enum => struct {
-                alias: []const u8 = "",
-                //bound: ,
-                other: bool = false,
-                rename: []const u8 = @typeName(T),
-                rename_all: []const u8 = "",
-                skip: bool = false,
-                with: []const u8 = "",
-            },
-            else => unreachable,
+        .Enum => struct {
+            // Deserialize this variant from the given names *or* its type
+            // name.
+            alias: ?[][]const u8 = null,
+
+            // Deserialize this variant using a function that is different
+            // from the normal deserialization implementation.
+            deserialize_with: ?[]const u8 = null,
+
+            // Serialize and deserialize this variant with the given name
+            // instead of its type name.
+            rename: ?[]const u8 = null,
+
+            // Deserialize this variant with the given name instead of its
+            // type name.
+            //
+            // Overrides `rename`.
+            rename_de: ?[]const u8 = null,
+
+            // Serialize this variant with the given name instead of its
+            // type name.
+            //
+            // Overrides `rename`.
+            rename_ser: ?[]const u8 = null,
+
+            // Never serialize or deserialize this variant.
+            skip: bool = false,
+
+            // Never serialize this variant.
+            //
+            // Overrides `skip`.
+            skip_de: bool = false,
+
+            // Never deserialize this variant.
+            //
+            // Overrides `skip`.
+            skip_ser: bool = false,
+
+            // Serialize this variant using a function that is different from
+            // the normal serialization implementation.
+            serialize_with: ?[]const u8 = null,
+
+            // Combination of serialize_with and deserialize_with.
+            with: ?[]const u8 = null,
         },
+        else => unreachable,
     };
 
     const container = Container{};
@@ -190,7 +313,7 @@ fn _Attributes(comptime T: type, comptime mode: Mode) type {
 
 test "Serialize - basic (struct)" {
     const TestPoint = struct {
-        usingnamespace Attributes(@This(), .ser, .{});
+        usingnamespace Attributes(@This(), .{});
 
         x: i32,
         y: i32,
@@ -199,7 +322,9 @@ test "Serialize - basic (struct)" {
 
 test "Serialize - with container attribute (struct)" {
     const TestPoint = struct {
-        usingnamespace Attributes(@This(), .ser, .{ .TestPoint = .{ .rename = "A" } });
+        usingnamespace Attributes(@This(), .{
+            .TestPoint = .{ .rename = "A" },
+        });
 
         x: i32,
         y: i32,
@@ -208,7 +333,9 @@ test "Serialize - with container attribute (struct)" {
 
 test "Serialize - with field attribute (struct)" {
     const TestPoint = struct {
-        usingnamespace Attributes(@This(), .ser, .{ .x = .{ .rename = "a" } });
+        usingnamespace Attributes(@This(), .{
+            .x = .{ .rename = "a" },
+        });
 
         x: i32,
         y: i32,
