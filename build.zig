18b1600d1b3c1d7e3c847d90a472db8d450ab8b2
Jason Phan <jason@ket.so>
2021-05-22 13:53:34 -0500

    build: use standard release options instead of a build mode array

--- build.zig
+++ build.zig
@@ -5,8 +5,6 @@ const Builder = std.build.Builder;
 const package_name = "getty";
 const package_path = "src/lib.zig";
 
-const modes = [_]std.builtin.Mode{.Debug};
-
 const test_files = [_][]const u8{
     "src/attributes.zig",
     "src/ser.zig",
@@ -17,33 +15,21 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
-    tests(b, target);
+    tests(b, mode, target);
     library(b, mode);
 }
 
-fn tests(b: *Builder, target: std.zig.CrossTarget) void {
-    const test_step = b.step("test", "Run library tests.");
+fn tests(b: *Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget) void {
+    const step = b.step("test", "Run library tests.");
 
-    inline for (&modes) |m| {
-        const mode = switch (m) {
-            .Debug => "DEBUG",
-            .ReleaseSafe => "RELEASE-SAFE",
-            .ReleaseFast => "RELEASE-FAST",
-            .ReleaseSmall => "RELEASE-SMALL",
-        };
+    inline for (&test_files) |f| {
+        const t = b.addTest(f);
 
-        const step = b.step("(" ++ mode ++ ")", "Run tests in " ++ mode ++ " mode.");
-        test_step.dependOn(step);
+        t.addPackagePath(package_name, package_path);
+        t.setBuildMode(mode);
+        t.setTarget(target);
 
-        inline for (&test_files) |f| {
-            const t = b.addTest(f);
-
-            t.addPackagePath(package_name, package_path);
-            t.setBuildMode(m);
-            t.setTarget(target);
-
-            step.dependOn(&t.step);
-        }
+        step.dependOn(&t.step);
     }
 }
 
