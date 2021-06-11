79e75edbbc4cfe20cf0f5013c31fe2bce47d2ba8
Jason Phan <jason@ket.so>
2021-06-11 15:44:59 -0500

    de: Add deserializeInt

--- src/de.zig
+++ src/de.zig
@@ -14,7 +14,7 @@ pub fn Deserializer(
     comptime E: type,
     //comptime anyFn: DeserializerFn(Context, E),
     comptime boolFn: DeserializerFn(Context, E),
-    //comptime intFn: DeserializerFn(Context, E),
+    comptime intFn: DeserializerFn(Context, E),
     //comptime floatFn: DeserializerFn(Context, E),
     //comptime optionFn: DeserializerFn(Context, E),
     //comptime sequenceFn: DeserializerFn(Context, E),
@@ -37,9 +37,9 @@ pub fn Deserializer(
             return try boolFn(self.context, visitor);
         }
 
-        //pub fn deserializeInt(self: Self, visitor: anytype) E!@TypeOf(visitor).Ok {
-        //return try intFn(self.context, visitor);
-        //}
+        pub fn deserializeInt(self: Self, visitor: anytype) E!@TypeOf(visitor).Ok {
+            return try intFn(self.context, visitor);
+        }
 
         //pub fn deserializeFloat(self: Self, visitor: anytype) E!@TypeOf(visitor).Ok {
         //return try floatFn(self.context, visitor);
@@ -74,7 +74,7 @@ pub fn Visitor(
     comptime O: type,
     comptime E: type,
     comptime boolFn: fn (Context, bool) E!O,
-    //comptime intFn: fn (Context, anytype) E!O,
+    comptime intFn: fn (Context, anytype) E!O,
     //comptime floatFn: fn (Context, anytype) E!O,
     //comptime nullFn: fn (Context, anytype) E!O,
     //comptime someFn: fn (Context, anytype) E!O,
@@ -94,37 +94,92 @@ pub fn Visitor(
         pub fn visitBool(self: Self, value: bool) E!O {
             return try boolFn(self.context, value);
         }
+
+        pub fn visitInt(self: Self, value: anytype) E!O {
+            return try intFn(self.context, value);
+        }
+
+        //pub fn visitFloat(self: Self, value: anytype) E!O {
+        //return try floatFn(self.context, value);
+        //}
+
+        //pub fn visitNull(self: Self, value: anytype) E!O {
+        //return try nullFn(self.context, value);
+        //}
+
+        //pub fn visitSome(self: Self, value: anytype) E!O {
+        //return try someFn(self.context, value);
+        //}
+
+        //pub fn visitSequence(self: Self, value: anytype) E!O {
+        //return try sequenceFn(self.context, value);
+        //}
+
+        //pub fn visitString(self: Self, value: anytype) E!O {
+        //return try stringFn(self.context, value);
+        //}
+
+        //pub fn visitStruct(self: Self, value: anytype) E!O {
+        //return try structFn(self.context, value);
+        //}
+
+        //pub fn visitVariant(self: Self, value: anytype) E!O {
+        //return try variantFn(self.context, value);
+        //}
     };
 }
 
-/// Visitable implementation
 const TestDeserializer = struct {
     input: []const u8,
 
     const Self = @This();
 
-    const Error = error{DeserializationError};
+    pub const Error = error{DeserializationError};
 
-    const D = Deserializer(
+    /// Implements `getty.de.Deserializer`.
+    pub const D = Deserializer(
         *Self,
         Error,
         deserializeBool,
+        deserializeInt,
     );
 
     pub fn deserializer(self: *Self) D {
         return .{ .context = self };
     }
 
+    /// Implements `boolFn` for `getty.de.Deserializer`.
     pub fn deserializeBool(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Ok {
-        return visitor.visitBool(true) catch return Error.DeserializationError;
+        if (std.mem.eql(u8, self.input, "true")) {
+            return visitor.visitBool(true) catch return Error.DeserializationError;
+        } else if (std.mem.eql(u8, self.input, "false")) {
+            return visitor.visitBool(false) catch return Error.DeserializationError;
+        }
+
+        return Error.DeserializationError;
     }
+
+    /// Implements `intFn` for `getty.de.Deserializer`.
+    pub fn deserializeInt(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Ok {
+        const value = std.json.parse(i64, &std.json.TokenStream.init(self.input), .{}) catch return Error.DeserializationError;
+        return visitor.visitInt(value) catch return Error.DeserializationError;
+    }
+
+    // TODO: How do we know what size float to parse from the input?
+    //pub fn deserializeFloat(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Ok {
+    //const value: f64 = parseFloat(f64, self.input) catch unreachable;
+    //return visitor.visitFloat(value) catch return Error.DeserializationError;
+    //}
+
+    //pub fn deserializeOption(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Ok {
+    //return visitor.visitOption(value) catch return Error.DeserializationError;
+    //}
 };
 
 /// Client data structure
 const PublishState = enum {
     Published,
     Unpublished,
-    Pending,
 };
 
 /// Visitor implementation
@@ -139,6 +194,7 @@ const PublishStateVisitor = struct {
         Ok,
         Error,
         visitBool,
+        visitInt,
     );
 
     pub fn visitor(self: *Self) V {
@@ -151,14 +207,23 @@ const PublishStateVisitor = struct {
             false => .Unpublished,
         };
     }
+
+    fn visitInt(self: *Self, value: anytype) Error!Ok {
+        if (value > 0) {
+            return .Published;
+        } else {
+            return .Unpublished;
+        }
+    }
 };
 
-test {
+test "bool" {
+    var print_visitor = PublishStateVisitor{};
+    const visitor = print_visitor.visitor();
+
     {
         var test_deserializer = TestDeserializer{ .input = "true" };
         const deserializer = test_deserializer.deserializer();
-        var print_visitor = PublishStateVisitor{};
-        const visitor = print_visitor.visitor();
 
         var publish_state = try deserializer.deserializeBool(visitor);
         try std.testing.expect(publish_state == .Published);
@@ -167,8 +232,6 @@ test {
     {
         var test_deserializer = TestDeserializer{ .input = "false" };
         const deserializer = test_deserializer.deserializer();
-        var print_visitor = PublishStateVisitor{};
-        const visitor = print_visitor.visitor();
 
         var publish_state = try deserializer.deserializeBool(visitor);
         try std.testing.expect(publish_state == .Unpublished);
@@ -177,14 +240,41 @@ test {
     {
         var test_deserializer = TestDeserializer{ .input = "" };
         const deserializer = test_deserializer.deserializer();
-        var print_visitor = PublishStateVisitor{};
-        const visitor = print_visitor.visitor();
 
         if (deserializer.deserializeBool(visitor)) |_| {
             unreachable;
         } else |err| {
             try std.testing.expect(err == TestDeserializer.Error.DeserializationError);
         }
+    }
+}
+
+test "int" {
+    var print_visitor = PublishStateVisitor{};
+    const visitor = print_visitor.visitor();
+
+    {
+        var test_deserializer = TestDeserializer{ .input = "0" };
+        const deserializer = test_deserializer.deserializer();
+
+        var publish_state = try deserializer.deserializeInt(visitor);
+        try std.testing.expect(publish_state == .Unpublished);
+    }
+
+    {
+        var test_deserializer = TestDeserializer{ .input = "1" };
+        const deserializer = test_deserializer.deserializer();
+
+        var publish_state = try deserializer.deserializeInt(visitor);
+        try std.testing.expect(publish_state == .Published);
+    }
+
+    {
+        var test_deserializer = TestDeserializer{ .input = "-1" };
+        const deserializer = test_deserializer.deserializer();
+
+        var publish_state = try deserializer.deserializeInt(visitor);
+        try std.testing.expect(publish_state == .Unpublished);
     }
 }
 
