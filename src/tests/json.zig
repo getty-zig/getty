7787139d87c1952c0379d1f61a75bd4dd5b929e7
Jason Phan <jason@ket.so>
2021-07-10 13:14:33 -0500

    json: Refactor tests

--- src/tests/json.zig
+++ src/tests/json.zig
@@ -1,59 +1,47 @@
 const std = @import("std");
 const getty_json = @import("getty").json;
 
-const expectEqualSlices = std.testing.expectEqualSlices;
-const String = std.ArrayList(u8);
-
-test "toWriter" {
-    const test_cases = [_]struct { input: anytype, output: []const u8 }{
-        // Bool
-        .{ .input = true, .output = "true" },
-        .{ .input = false, .output = "false" },
-
-        // Integer
-        .{ .input = 'A', .output = "65" },
-        .{ .input = comptime std.math.maxInt(u32), .output = "4294967295" },
-        .{ .input = comptime std.math.maxInt(u64), .output = "18446744073709551615" },
-        .{ .input = comptime std.math.minInt(i32), .output = "-2147483648" },
-
-        // Null
-        .{ .input = null, .output = "null" },
-
-        // String
-        .{ .input = "Hello, World!", .output = "\"Hello, World!\"" },
-
-        // Enum
-        .{ .input = enum { Foo }.Foo, .output = "\"Foo\"" },
-        .{ .input = .Foo, .output = "\"Foo\"" },
-    };
-
-    inline for (test_cases) |t| {
-        var array_list = String.init(std.testing.allocator);
-        defer array_list.deinit();
-
-        try getty_json.toWriter(array_list.writer(), t.input);
-        try expectEqualSlices(u8, array_list.items, t.output);
-    }
+test "toWriter - Array" {
+    try t([_]i8{ 1, 2, 3 }, "[1,2,3]");
 }
 
-// FIXME: Merge into test "toWriter" blocked by #5877.
-test "toWriter - array" {
-    var array_list = String.init(std.testing.allocator);
-    defer array_list.deinit();
-
-    try getty_json.toWriter(array_list.writer(), [_]u8{ 1, 2, 3 });
-    try expectEqualSlices(u8, array_list.items, "[1,2,3]");
+test "toWriter - Bool" {
+    try t(true, "true");
+    try t(false, "false");
 }
 
-// FIXME: Merge into test "toWriter" blocked by #5877.
-test "toWriter - struct" {
-    var array_list = String.init(std.testing.allocator);
-    defer array_list.deinit();
+test "toWriter - Enum" {
+    try t(enum { Foo }.Foo, "\"Foo\"");
+    try t(.Foo, "\"Foo\"");
+}
 
+test "toWriter - Integer" {
+    try t('A', "65");
+    try t(std.math.maxInt(u32), "4294967295");
+    try t(std.math.maxInt(u64), "18446744073709551615");
+    try t(std.math.minInt(i32), "-2147483648");
+    try t(std.math.maxInt(i64), "9223372036854775807");
+}
+
+test "toWriter - Null" {
+    try t(null, "null");
+}
+
+test "toWriter - String" {
+    try t("Foobar", "\"Foobar\"");
+}
+
+test "toWriter - Struct" {
     const Point = struct { x: i32, y: i32 };
-    var point = Point{ .x = 1, .y = 2 };
+    const point = Point{ .x = 1, .y = 2 };
 
-    try getty_json.toWriter(array_list.writer(), point);
+    try t(point, "{\"x\":1,\"y\":2}");
+}
 
-    try expectEqualSlices(u8, array_list.items, "{\"x\":1,\"y\":2}");
+fn t(input: anytype, output: []const u8) !void {
+    var array_list = std.ArrayList(u8).init(std.testing.allocator);
+    defer array_list.deinit();
+
+    try getty_json.toWriter(array_list.writer(), input);
+    try std.testing.expectEqualSlices(u8, array_list.items, output);
 }
