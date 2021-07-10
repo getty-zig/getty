e749707e5a2adb039bfd59cb0d3a6f42ee225dc4
Jason Phan <jason@ket.so>
2021-07-10 13:03:32 -0500

    ser: Refactor tests
    Go approach is ugly and gross.

--- src/tests/ser.zig
+++ src/tests/ser.zig
@@ -169,111 +169,47 @@ pub const Serializer = struct {
     }
 };
 
-// TODO: Handle multiple serializations (e.g., .{ .Int, .Int, ... }).
-test "Serialize" {
-    const test_cases = [_]struct {
-        name: []const u8,
-        input: anytype,
-        output: []const Elem,
-    }{
-        .{
-            .name = "Boolean",
-            .input = true,
-            .output = &.{ .Bool, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Error value",
-            .input = error.Elem,
-            .output = &.{ .String, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Integer (comptime)",
-            .input = 1,
-            .output = &.{ .Int, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Integer (unsigned)",
-            .input = @as(u8, 1),
-            .output = &.{ .Int, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Integer (signed)",
-            .input = @as(i8, -1),
-            .output = &.{ .Int, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Null",
-            .input = null,
-            .output = &.{ .Null, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Optional (some)",
-            .input = @as(?i8, 1),
-            .output = &.{ .Int, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Optional (none)",
-            .input = @as(?i8, null),
-            .output = &.{ .Null, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "String literal",
-            .input = "h\x65llo",
-            .output = &.{ .String, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "String (byte slice)",
-            .input = &[_]u8{65},
-            .output = &.{ .String, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Enum (variant instance)",
-            .input = enum { Foo }.Foo,
-            .output = &.{ .Variant, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Enum (literal)",
-            .input = .Foo,
-            .output = &.{ .Variant, .Undefined, .Undefined, .Undefined },
-        },
-        .{
-            .name = "Vector",
-            .input = @splat(2, @as(u32, 1)),
-            .output = &.{ .SequenceStart, .Element, .Element, .SequenceEnd },
-        },
+test "Array" {
+    try t([_]i8{ 1, 2 }, &.{ .SequenceStart, .Element, .Element, .SequenceEnd });
+}
+
+test "Boolean" {
+    try t(true, &.{ .Bool, .Undefined, .Undefined, .Undefined });
+    try t(false, &.{ .Bool, .Undefined, .Undefined, .Undefined });
+}
+
+test "Enum" {
+    try t(enum { Foo }.Foo, &.{ .Variant, .Undefined, .Undefined, .Undefined });
+    try t(.Foo, &.{ .Variant, .Undefined, .Undefined, .Undefined });
+}
+
+test "Error value" {
+    try t(error.Elem, &.{ .String, .Undefined, .Undefined, .Undefined });
+}
+
+test "Integer" {
+    try t(1, &.{ .Int, .Undefined, .Undefined, .Undefined });
+    try t(@as(u8, 1), &.{ .Int, .Undefined, .Undefined, .Undefined });
+    try t(@as(i8, -1), &.{ .Int, .Undefined, .Undefined, .Undefined });
+}
+
+test "Optional" {
+    try t(@as(?bool, true), &.{ .Bool, .Undefined, .Undefined, .Undefined });
+    try t(@as(?bool, null), &.{ .Null, .Undefined, .Undefined, .Undefined });
+}
+
+test "String" {
+    try t("h\x65llo", &.{ .String, .Undefined, .Undefined, .Undefined });
+    try t(&[_]u8{65}, &.{ .String, .Undefined, .Undefined, .Undefined });
+}
+
+test "Struct" {
+    const A = struct {
+        x: i32,
+        y: i32,
     };
 
-    inline for (test_cases) |t| {
-        var s = Serializer{};
-        try ser.serialize(&s, t.input);
-        try expectEqualSlices(Elem, &s.buf, t.output);
-    }
-}
-
-// FIXME: Merge into test "Serialize" blocked by #5877.
-test "Serialize - array" {
-    var s = Serializer{};
-    try ser.serialize(&s, [_]u8{ 1, 2 });
-    try expectEqualSlices(Elem, &s.buf, &.{
-        .SequenceStart,
-        .Element,
-        .Element,
-        .SequenceEnd,
-    });
-}
-
-// FIXME: Merge into test "Serialize" blocked by #5877.
-test "Serialize - struct" {
-    var s = Serializer{};
-    try ser.serialize(&s, struct { x: i32, y: i32 }{ .x = 0, .y = 0 });
-    try expectEqualSlices(Elem, &s.buf, &.{ .StructStart, .Field, .Field, .StructEnd });
-}
-
-// FIXME: Merge into test "Serialize" blocked by #5877.
-test "Serialize - struct (custom)" {
-    var s = Serializer{};
-
-    const Point = struct {
+    const B = struct {
         x: i32,
         y: i32,
 
@@ -285,17 +221,31 @@ test "Serialize - struct (custom)" {
         }
     };
 
-    var point = Point{ .x = 1, .y = 2 };
+    const a = A{ .x = 0, .y = 0 };
+    const b = B{ .x = 0, .y = 0 };
 
-    try ser.serialize(&s, &point);
-    try expectEqualSlices(Elem, &s.buf, &.{ .StructStart, .Field, .StructEnd, .Undefined });
+    try t(a, &.{ .StructStart, .Field, .Field, .StructEnd });
+    try t(b, &.{ .StructStart, .Field, .StructEnd, .Undefined });
 }
 
-// FIXME: Merge into test "Serialize" blocked by #5877 (probably).
-test "Serialize - tagged union" {
-    const Union = union(enum) { Int: i32, Bool: bool };
+test "Tagged union" {
+    const Union = union(enum) { Int: u8, Bool: bool };
+
+    try t(Union{ .Int = 0 }, &.{ .Int, .Undefined, .Undefined, .Undefined });
+    try t(Union{ .Bool = true }, &.{ .Bool, .Undefined, .Undefined, .Undefined });
+}
+
+test "Vector" {
+    try t(@splat(2, @as(u32, 1)), &.{ .SequenceStart, .Element, .Element, .SequenceEnd });
+}
+
+fn t(input: anytype, output: []const Elem) !void {
     var s = Serializer{};
-    try ser.serialize(&s, Union{ .Int = 42 });
-    try ser.serialize(&s, Union{ .Bool = true });
-    try expectEqualSlices(Elem, &s.buf, &.{ .Int, .Bool, .Undefined, .Undefined });
+    try ser.serialize(&s, input);
+
+    try expectEqualSlices(Elem, &s.buf, output);
+}
+
+comptime {
+    std.testing.refAllDecls(@This());
 }
