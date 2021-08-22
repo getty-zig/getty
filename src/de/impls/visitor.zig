1a528025ac93864e1e1ade90f9465c4f3fd756d8
Jason Phan <jason@ket.so>
2021-08-22 09:27:13 -0500

    de: Move visitor imports into lib.zig
    The library should be defining what's named what anyways. Plus,
    now we don't have an ugly visitor.zig file lying around.

--- src/de/impls/visitor.zig
+++
@@ -1,4 +0,0 @@
-pub const BoolVisitor = @import("visitors/bool.zig");
-pub const FloatVisitor = @import("visitors/float.zig").Visitor;
-pub const IntVisitor = @import("visitors/int.zig").Visitor;
-pub const VoidVisitor = @import("visitors/void.zig");
--- src/lib.zig
+++ src/lib.zig
@@ -1,19 +1,24 @@
 pub const de = struct {
+    // Interfaces
     pub usingnamespace @import("de/interfaces/access.zig");
     pub usingnamespace @import("de/interfaces/deserializer.zig");
     pub usingnamespace @import("de/interfaces/seed.zig");
     pub usingnamespace @import("de/interfaces/visitor.zig");
 
+    // Implementations
     pub usingnamespace @import("de/impls/seed.zig");
-    pub usingnamespace @import("de/impls/visitor.zig");
+    pub const BoolVisitor = @import("de/impls/visitors/bool.zig");
+    pub const FloatVisitor = @import("de/impls/visitors/float.zig").Visitor;
+    pub const IntVisitor = @import("de/impls/visitors/int.zig").Visitor;
+    pub const VoidVisitor = @import("de/impls/visitors/void.zig");
 };
 
 pub const ser = struct {
+    // Interfaces
     pub usingnamespace @import("ser/interfaces/map.zig");
     pub usingnamespace @import("ser/interfaces/serializer.zig");
     pub usingnamespace @import("ser/interfaces/sequence.zig");
     pub usingnamespace @import("ser/interfaces/structure.zig");
-
     pub const Tuple = Sequence;
 };
 
