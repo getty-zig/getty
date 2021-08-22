7f592afeb7438ff2a84370dfc9d78e22e30db4ab
Jason Phan <jason@ket.so>
2021-08-22 09:12:18 -0500

    de: Add unsupport panics for FloatVisitor

--- src/de/impls/visitors/float.zig
+++ src/de/impls/visitors/float.zig
@@ -5,21 +5,26 @@ pub fn FloatVisitor(comptime T: type) type {
         const Self = @This();
 
         /// Implements `getty.de.Visitor`.
-        pub fn visitor(self: *Self) de.Visitor(
+        const V = de.Visitor(
             *Self,
-            T,
-            undefined,
+            Value,
+            visitBool,
             visitFloat,
             visitInt,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-        ) {
-            return .{ .context = self };
+            visitMap,
+            visitNull,
+            visitSequence,
+            visitSome,
+            visitString,
+            visitVariant,
+            visitVoid,
+        );
+
+        fn visitBool(self: *Self, comptime Error: type, input: bool) Error!Value {
+            _ = self;
+            _ = input;
+
+            @panic("Unsupported");
         }
 
         fn visitFloat(self: *Self, comptime Error: type, input: anytype) Error!Value {
@@ -36,6 +41,50 @@ pub fn FloatVisitor(comptime T: type) type {
             comptime std.debug.assert(@typeInfo(@TypeOf(input)) == .Int);
 
             return @intToFloat(T, value);
+        }
+
+        fn visitMap(self: *Self, mapAccess: anytype) @TypeOf(mapAccess).Error!Value {
+            _ = self;
+
+            @panic("Unsupported");
+        }
+
+        fn visitNull(self: *Self, comptime Error: type) Error!Value {
+            _ = self;
+
+            @panic("Unsupported");
+        }
+
+        fn visitSequence(self: *Self, sequenceAccess: anytype) @TypeOf(sequenceAccess).Error!Value {
+            _ = self;
+
+            @panic("Unsupported");
+        }
+
+        fn visitSome(self: *Self, deserializer: anytype) @TypeOf(deserializer).Error!Value {
+            _ = self;
+
+            @panic("Unsupported");
+        }
+
+        fn visitString(self: *Self, comptime Error: type, input: anytype) Error!Value {
+            _ = self;
+            _ = input;
+
+            @panic("Unsupported");
+        }
+
+        fn visitVariant(self: *Self, comptime Error: type, input: anytype) Error!Value {
+            _ = self;
+            _ = input;
+
+            @panic("Unsupported");
+        }
+
+        fn visitVoid(self: *Self, comptime Error: type) Error!Value {
+            _ = self;
+
+            @panic("Unsupported");
         }
     };
 }
