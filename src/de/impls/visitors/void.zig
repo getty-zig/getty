800e94bf5d2c5cf446b72a2f936142cb96288e4b
Jason Phan <jason@ket.so>
2021-08-22 09:08:41 -0500

    de: Add unsupport panics for VoidVisitor

--- src/de/impls/visitors/void.zig
+++ src/de/impls/visitors/void.zig
@@ -12,17 +12,76 @@ pub fn visitor(self: *Self) V {
 const V = de.Visitor(
     *Self,
     Value,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
+    visitBool,
+    visitFloat,
+    visitInt,
+    visitMap,
+    visitNull,
+    visitSequence,
+    visitSome,
+    visitString,
+    visitVariant,
     visitVoid,
 );
+
+fn visitBool(self: *Self, comptime Error: type, input: bool) Error!Value {
+    _ = self;
+    _ = input;
+
+    @panic("Unsupported");
+}
+
+fn visitInt(self: *Self, comptime Error: type, input: anytype) Error!Value {
+    _ = self;
+    _ = input;
+
+    @panic("Unsupported");
+}
+
+fn visitFloat(self: *Self, comptime Error: type, input: anytype) Error!Value {
+    _ = self;
+    _ = input;
+
+    @panic("Unsupported");
+}
+
+fn visitMap(self: *Self, mapAccess: anytype) @TypeOf(mapAccess).Error!Value {
+    _ = self;
+
+    @panic("Unsupported");
+}
+
+fn visitNull(self: *Self, comptime Error: type) Error!Value {
+    _ = self;
+
+    @panic("Unsupported");
+}
+
+fn visitSequence(self: *Self, sequenceAccess: anytype) @TypeOf(sequenceAccess).Error!Value {
+    _ = self;
+
+    @panic("Unsupported");
+}
+
+fn visitSome(self: *Self, deserializer: anytype) @TypeOf(deserializer).Error!Value {
+    _ = self;
+
+    @panic("Unsupported");
+}
+
+fn visitString(self: *Self, comptime Error: type, input: anytype) Error!Value {
+    _ = self;
+    _ = input;
+
+    @panic("Unsupported");
+}
+
+fn visitVariant(self: *Self, comptime Error: type, input: anytype) Error!Value {
+    _ = self;
+    _ = input;
+
+    @panic("Unsupported");
+}
 
 fn visitVoid(self: *Self, comptime Error: type) Error!Value {
     _ = self;
