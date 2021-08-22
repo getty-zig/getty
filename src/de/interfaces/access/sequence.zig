05f689da67c78e874d1f244d69c974a3191dac18
Jason Phan <jason@ket.so>
2021-08-22 09:23:08 -0500

    de: Move accesses to their own files

---
+++ src/de/interfaces/access/sequence.zig
@@ -0,0 +1,31 @@
+const Seed = @import("../../../lib.zig").de.Seed;
+
+pub fn Access(
+    comptime Context: type,
+    comptime E: type,
+    comptime nextElementSeedFn: @TypeOf(struct {
+        fn f(c: Context, seed: anytype) E!?@TypeOf(seed).Value {
+            _ = c;
+            unreachable;
+        }
+    }.f),
+) type {
+    return struct {
+        context: Context,
+
+        const Self = @This();
+
+        pub const Error = E;
+
+        pub fn nextElementSeed(self: Self, seed: anytype) Error!?@TypeOf(seed).Value {
+            return try nextElementSeedFn(self.context, seed);
+        }
+
+        pub fn nextElement(self: Self, comptime Value: type) Error!?Value {
+            var seed = Seed(Value){};
+            const ds = seed.deserializeSeed();
+
+            return (try self.nextElementSeed(ds)).?;
+        }
+    };
+}
