298aae724a781c61edf812084d441b0f2cf31932
Jason Phan <jason@ket.so>
2021-08-27 21:51:18 -0500

    Update Gyro package files

---
+++ deps.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+pub const pkgs = struct {
+    pub fn addAllTo(artifact: *std.build.LibExeObjStep) void {
+        @setEvalBranchQuota(1_000_000);
+        inline for (std.meta.declarations(pkgs)) |decl| {
+            if (decl.is_pub and decl.data == .Var) {
+                artifact.addPackage(@field(pkgs, decl.name));
+            }
+        }
+    }
+};
+
+pub const exports = struct {
+    pub const getty = std.build.Pkg{
+        .name = "getty",
+        .path = .{ .path = "src/lib.zig" },
+        .dependencies = &.{
+        },
+    };
+};
+pub const base_dirs = struct {
+};
