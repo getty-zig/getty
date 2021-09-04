350bad728ec5d8edfd76176ed4742e348fcd227f
Jason Phan <jason@ket.so>
2021-09-04 16:32:37 -0500

    Clean up lib.zig a bit

--- src/lib.zig
+++ src/lib.zig
@@ -1,20 +1,17 @@
-const testing = @import("std").testing;
-
 pub const de = struct {
     pub usingnamespace @import("de.zig").interface;
     pub usingnamespace @import("de.zig").impl;
 };
-
-pub const deserialize = @import("de.zig").deserialize;
 
 pub const ser = struct {
     pub usingnamespace @import("ser.zig").interface;
     pub usingnamespace @import("ser.zig").impl;
 };
 
+pub const deserialize = @import("de.zig").deserialize;
 pub const serialize = @import("ser.zig").serialize;
 pub const serializeWith = @import("ser.zig").serializeWith;
 
 test {
-    testing.refAllDecls(@This());
+    @import("std").testing.refAllDecls(@This());
 }
