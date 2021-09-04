03348f4b7b7dc43a8a2d144580a096aa10b12273
Jason Phan <jason@ket.so>
2021-09-04 13:16:54 -0500

    Reorganize (II)
    de imports and 'deserialize' is moved to src/de.zig to match std layout.

---
+++ src/de.zig
@@ -0,0 +1,40 @@
+// Interfaces
+pub const Deserializer = @import("de/interfaces/deserializer.zig").Deserializer;
+pub const DeserializeSeed = @import("de/interfaces/seed.zig").DeserializeSeed;
+pub const SequenceAccess = @import("de/interfaces/access/sequence.zig").Access;
+pub const Visitor = @import("de/interfaces/visitor.zig").Visitor;
+
+// Implementations
+pub const Seed = @import("de/impls/seed.zig").Seed;
+
+pub const BoolVisitor = @import("de/impls/visitors/bool.zig");
+pub const FloatVisitor = @import("de/impls/visitors/float.zig").Visitor;
+pub const IntVisitor = @import("de/impls/visitors/int.zig").Visitor;
+pub const OptionalVisitor = @import("de/impls/visitors/optional.zig").Visitor;
+pub const VoidVisitor = @import("de/impls/visitors/void.zig");
+
+pub fn deserialize(comptime T: type, deserializer: anytype) @TypeOf(deserializer).Error!T {
+    switch (@typeInfo(T)) {
+        .Bool => {
+            var visitor = BoolVisitor{};
+            return try deserializer.deserializeBool(visitor.visitor());
+        },
+        .Float, .ComptimeFloat => {
+            var visitor = FloatVisitor(T){};
+            return try deserializer.deserializeFloat(visitor.visitor());
+        },
+        .Int, .ComptimeInt => {
+            var visitor = IntVisitor(T){};
+            return try deserializer.deserializeInt(visitor.visitor());
+        },
+        .Optional => {
+            var visitor = OptionalVisitor(T){};
+            return try deserializer.deserializeOptional(visitor.visitor());
+        },
+        .Void => {
+            var visitor = VoidVisitor{};
+            return try deserializer.deserializeVoid(visitor.visitor());
+        },
+        else => unreachable,
+    }
+}
--- src/lib.zig
+++ src/lib.zig
@@ -1,25 +1,9 @@
 const testing = @import("std").testing;
 
-pub const de = struct {
-    // Interfaces
-    pub const Deserializer = @import("de/interfaces/deserializer.zig").Deserializer;
-    pub const DeserializeSeed = @import("de/interfaces/seed.zig").DeserializeSeed;
-    pub const Visitor = @import("de/interfaces/visitor.zig").Visitor;
-
-    pub const SequenceAccess = @import("de/interfaces/access/sequence.zig").Access;
-
-    // Implementations
-    pub const Seed = @import("de/impls/seed.zig").Seed;
-    pub const BoolVisitor = @import("de/impls/visitors/bool.zig");
-    pub const FloatVisitor = @import("de/impls/visitors/float.zig").Visitor;
-    pub const IntVisitor = @import("de/impls/visitors/int.zig").Visitor;
-    pub const OptionalVisitor = @import("de/impls/visitors/optional.zig");
-    pub const VoidVisitor = @import("de/impls/visitors/void.zig");
-};
-
+pub const de = @import("de.zig");
 pub const ser = @import("ser.zig");
 
-pub const deserialize = @import("de/deserialize.zig").deserialize;
+pub const deserialize = @import("de.zig").deserialize;
 pub const serialize = @import("ser.zig").serialize;
 pub const serializeWith = @import("ser.zig").serializeWith;
 
