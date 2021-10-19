68535775406cdeb0703bacf0a241b07c5eb6d32a
Jason Phan <jason@ket.so>
2021-10-19 08:28:29 -0500

    de: Rename mapSerialize variable in hash map ser impl

--- src/ser/impl/ser/hash_map.zig
+++ src/ser/impl/ser/hash_map.zig
@@ -6,12 +6,12 @@ pub usingnamespace getty.Ser(
 );
 
 fn serialize(_: *@This(), value: anytype, serializer: anytype) @TypeOf(serializer).Error!@TypeOf(serializer).Ok {
-    const st = (try serializer.serializeMap(value.count())).mapSerialize();
+    const m = (try serializer.serializeMap(value.count())).mapSerialize();
     {
         var iterator = value.iterator();
         while (iterator.next()) |entry| {
-            try st.serializeEntry(entry.key_ptr.*, entry.value_ptr.*);
+            try m.serializeEntry(entry.key_ptr.*, entry.value_ptr.*);
         }
     }
-    return try st.end();
+    return try m.end();
 }
