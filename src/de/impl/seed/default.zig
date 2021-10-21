39b196018829894790cca47ef1505ddf081eb8a8
Jason Phan <jason@ket.so>
2021-10-21 09:41:22 -0500

    de: Reogranize impls for DefaultSeed

--- src/de/impl/seed/default.zig
+++ src/de/impl/seed/default.zig
@@ -4,18 +4,27 @@ const getty = @import("../../../lib.zig");
 pub fn DefaultSeed(comptime Value: type) type {
     return struct {
         const Self = @This();
+        const impl = @"impl DefaultSeed";
 
         /// Implements `getty.de.Seed`.
         pub usingnamespace getty.de.Seed(
             *Self,
-            Value,
-            deserialize,
+            impl.seed(Value).Value,
+            impl.seed(Value).deserialize,
         );
-
-        fn deserialize(self: *Self, allocator: ?*std.mem.Allocator, deserializer: anytype) @TypeOf(deserializer).Error!Value {
-            _ = self;
-
-            return try getty.deserialize(allocator, Value, deserializer);
-        }
     };
 }
+
+const @"impl DefaultSeed" = struct {
+    fn seed(comptime V: type) type {
+        return struct {
+            const Value = V;
+
+            fn deserialize(self: *DefaultSeed(V), allocator: ?*std.mem.Allocator, deserializer: anytype) @TypeOf(deserializer).Error!Value {
+                _ = self;
+
+                return try getty.deserialize(allocator, Value, deserializer);
+            }
+        };
+    }
+};
