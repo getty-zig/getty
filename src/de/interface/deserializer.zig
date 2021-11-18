4b84e15b9eb63654aa5d65945dba88d7d6f27b3a
Jason Phan <jason@ket.so>
2021-11-18 16:04:31 -0600

    de: Give the interface type a name

--- src/de/interface/deserializer.zig
+++ src/de/interface/deserializer.zig
@@ -33,56 +33,56 @@ pub fn Deserializer(
     comptime structFn: Fn(Context, E),
     comptime voidFn: Fn(Context, E),
 ) type {
-    const T = struct {
-        context: Context,
-
-        const Self = @This();
-
-        pub const Error = E;
-
-        pub fn deserializeBool(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try boolFn(self.context, visitor);
-        }
-
-        pub fn deserializeEnum(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try enumFn(self.context, visitor);
-        }
-
-        pub fn deserializeFloat(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try floatFn(self.context, visitor);
-        }
-
-        pub fn deserializeInt(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try intFn(self.context, visitor);
-        }
-
-        pub fn deserializeMap(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try mapFn(self.context, visitor);
-        }
-
-        pub fn deserializeOptional(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try optionalFn(self.context, visitor);
-        }
-
-        pub fn deserializeSequence(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try sequenceFn(self.context, visitor);
-        }
-
-        pub fn deserializeString(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try stringFn(self.context, visitor);
-        }
-
-        pub fn deserializeStruct(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try structFn(self.context, visitor);
-        }
-
-        pub fn deserializeVoid(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
-            return try voidFn(self.context, visitor);
-        }
-    };
-
     return struct {
-        pub fn deserializer(self: Context) T {
+        pub const @"getty.Deserializer" = struct {
+            context: Context,
+
+            const Self = @This();
+
+            pub const Error = E;
+
+            pub fn deserializeBool(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try boolFn(self.context, visitor);
+            }
+
+            pub fn deserializeEnum(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try enumFn(self.context, visitor);
+            }
+
+            pub fn deserializeFloat(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try floatFn(self.context, visitor);
+            }
+
+            pub fn deserializeInt(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try intFn(self.context, visitor);
+            }
+
+            pub fn deserializeMap(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try mapFn(self.context, visitor);
+            }
+
+            pub fn deserializeOptional(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try optionalFn(self.context, visitor);
+            }
+
+            pub fn deserializeSequence(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try sequenceFn(self.context, visitor);
+            }
+
+            pub fn deserializeString(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try stringFn(self.context, visitor);
+            }
+
+            pub fn deserializeStruct(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try structFn(self.context, visitor);
+            }
+
+            pub fn deserializeVoid(self: Self, visitor: anytype) E!@TypeOf(visitor).Value {
+                return try voidFn(self.context, visitor);
+            }
+        };
+
+        pub fn deserializer(self: Context) @"getty.Deserializer" {
             return .{ .context = self };
         }
     };
