4c353a5dc488272d233d0c039b2338ce98a7d7c6
Jason Phan <jason@ket.so>
2022-02-03 12:44:55 -0600

    ser: Fix slice with block

--- src/ser/with/slice.zig
+++ src/ser/with/slice.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn is(comptime T: type) bool {
-    return @typeInfo(T) == .Pointer and comptime !std.meta.trait.isZigString(T);
+    return @typeInfo(T) == .Pointer and @typeInfo(T).Pointer.size == .Slice and comptime !std.meta.trait.isZigString(T);
 }
 
 pub fn serialize(value: anytype, serializer: anytype) @TypeOf(serializer).Error!@TypeOf(serializer).Ok {
