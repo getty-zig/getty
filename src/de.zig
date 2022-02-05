6a9c5a1df6e8ddeac187f20f220b08f223572d40
Jason Phan <jason@ket.so>
2022-02-04 20:29:05 -0600

    de: Fix ambiguous visitor reference

--- src/de.zig
+++ src/de.zig
@@ -142,19 +142,18 @@ pub const de = struct {
     }
 
     const pointer_with = struct {
-        const Visitor = @import("de/impl/visitor/pointer.zig").Visitor;
+        const PointerVisitor = @import("de/impl/visitor/pointer.zig").Visitor;
+
         pub fn is(comptime T: type) bool {
             return @typeInfo(T) == .Pointer and @typeInfo(T).Pointer.size == .One and comptime !std.meta.trait.isZigString(T);
         }
-        pub fn visitor(allocator: ?std.mem.Allocator, comptime T: type) Visitor(T) {
+
+        pub fn visitor(allocator: ?std.mem.Allocator, comptime T: type) PointerVisitor(T) {
             return .{ .allocator = allocator.? };
         }
+
         pub fn deserialize(comptime T: type, deserializer: anytype, v: anytype) !@TypeOf(v).Value {
-            return try _deserialize(
-                std.meta.Child(T),
-                deserializer,
-                v,
-            );
+            return try _deserialize(std.meta.Child(T), deserializer, v);
         }
     };
 };
