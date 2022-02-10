2ee607038990544a2de840a29e0689ff4af3dc6a
Jason Phan <jason@ket.so>
2022-02-09 21:24:02 -0600

    ser: Remove concepts dependency from Structure concept

--- src/ser/concept/structure.zig
+++ src/ser/concept/structure.zig
@@ -1,14 +1,29 @@
 const std = @import("std");
 
-const concepts = @import("concepts");
+const concepts = @import("../../lib.zig").concepts;
 
 const concept = "getty.ser.Structure";
 
 pub fn @"getty.ser.Structure"(comptime T: type) void {
-    comptime concepts.Concept(concept, "")(.{
-        std.mem.eql(u8, @typeName(T), concept),
-        concepts.traits.hasField(T, "context"),
-        concepts.traits.hasDecls(T, .{ "Ok", "Error" }),
-        concepts.traits.hasFunctions(T, .{ "serializeField", "end" }),
-    });
+    comptime {
+        if (!std.meta.trait.isContainer(T) or !std.meta.trait.hasField("context")(T)) {
+            concepts.err(concept, "missing `context` field");
+        }
+
+        inline for (.{ "Ok", "Error" }) |decl| {
+            if (!@hasDecl(T, decl)) {
+                concepts.err(concept, "missing `" ++ decl ++ "` declaration");
+            }
+        }
+
+        inline for (.{ "serializeField", "end" }) |func| {
+            if (!std.meta.trait.hasFunctions(T, .{func})) {
+                concepts.err(concept, "missing `" ++ func ++ "` function");
+            }
+        }
+
+        if (!std.mem.eql(u8, @typeName(T), concept)) {
+            concepts.err(concept, "mismatched types");
+        }
+    }
 }
