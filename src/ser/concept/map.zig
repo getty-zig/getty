2b1f9884ac31d82ee2add43dd8ac2941efafd248
Jason Phan <jason@ket.so>
2022-02-09 21:22:01 -0600

    ser: Remove concepts dependency from Map concept

--- src/ser/concept/map.zig
+++ src/ser/concept/map.zig
@@ -1,19 +1,29 @@
 const std = @import("std");
 
-const concepts = @import("concepts");
+const concepts = @import("../../lib.zig").concepts;
 
 const concept = "getty.ser.Map";
 
 pub fn @"getty.ser.Map"(comptime T: type) void {
-    comptime concepts.Concept(concept, "")(.{
-        std.mem.eql(u8, @typeName(T), concept),
-        concepts.traits.hasField(T, "context"),
-        concepts.traits.hasDecls(T, .{ "Ok", "Error" }),
-        concepts.traits.hasFunctions(T, .{
-            "serializeKey",
-            "serializeValue",
-            "serializeEntry",
-            "end",
-        }),
-    });
+    comptime {
+        if (!std.meta.trait.isContainer(T) or !std.meta.trait.hasField("context")(T)) {
+            concepts.err(concept, "missing `context` field");
+        }
+
+        inline for (.{ "Ok", "Error" }) |decl| {
+            if (!@hasDecl(T, decl)) {
+                concepts.err(concept, "missing `" ++ decl ++ "` declaration");
+            }
+        }
+
+        inline for (.{ "serializeKey", "serializeValue", "end" }) |func| {
+            if (!std.meta.trait.hasFunctions(T, .{func})) {
+                concepts.err(concept, "missing `" ++ func ++ "` function");
+            }
+        }
+
+        if (!std.mem.eql(u8, @typeName(T), concept)) {
+            concepts.err(concept, "mismatched types");
+        }
+    }
 }
