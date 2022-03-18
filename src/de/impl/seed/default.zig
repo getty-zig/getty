c333593d2060e00b8ebd89ee2b314edfa63abee6
Jason Phan <jason@ket.so>
2022-03-18 14:12:00 -0500

    De: Remove impl block in default seed

--- src/de/impl/seed/default.zig
+++ src/de/impl/seed/default.zig
@@ -4,26 +4,15 @@ const getty = @import("../../../lib.zig");
 pub fn DefaultSeed(comptime Value: type) type {
     return struct {
         const Self = @This();
-        const impl = @"impl DefaultSeed"(Value);
 
         pub usingnamespace getty.de.Seed(
             Self,
-            impl.seed.Value,
-            impl.seed.deserialize,
+            Value,
+            deserialize,
         );
-    };
-}
 
-fn @"impl DefaultSeed"(comptime V: type) type {
-    const Self = DefaultSeed(V);
-
-    return struct {
-        pub const seed = struct {
-            pub const Value = V;
-
-            pub fn deserialize(_: Self, allocator: ?std.mem.Allocator, deserializer: anytype) @TypeOf(deserializer).Error!Value {
-                return try getty.deserialize(allocator, Value, deserializer);
-            }
-        };
+        fn deserialize(_: Self, allocator: ?std.mem.Allocator, deserializer: anytype) @TypeOf(deserializer).Error!Value {
+            return try getty.deserialize(allocator, Value, deserializer);
+        }
     };
 }
