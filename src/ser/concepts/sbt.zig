b59e830cdbed574673bba595209b5ac4fe149c11
Jason Phan <jphan@digitalocean.com>
2022-11-03 09:51:21 -0500

    ser: Update SBT concept/trait
    The code is simplified now. Checks for declarations and attributes has been added.

--- src/ser/concepts/sbt.zig
+++ src/ser/concepts/sbt.zig
@@ -5,49 +5,55 @@ const concepts = @import("../../lib.zig").concepts;
 const concept = "getty.ser.sbt";
 
 pub fn @"getty.ser.sbt"(comptime sbt: anytype) void {
-    const T = if (@TypeOf(sbt) == type) sbt else @TypeOf(sbt);
-    const info = @typeInfo(T);
-
     comptime {
+        const T = if (@TypeOf(sbt) == type) sbt else @TypeOf(sbt);
+        const info = @typeInfo(T);
+
         if (info == .Struct and info.Struct.is_tuple) {
-            inline for (std.meta.fields(T)) |field| {
-                const sb = @field(sbt, field.name);
-
-                if (@TypeOf(sb) != type) {
-                    concepts.err(concept, "found non-namespace Serialization Block");
-                }
-
-                switch (@typeInfo(sb)) {
-                    .Struct => |sb_info| {
-                        if (sb_info.is_tuple) {
-                            concepts.err(concept, "found non-namespace Serialization Block");
-                        }
-
-                        if (sb_info.fields.len != 0) {
-                            concepts.err(concept, "found field in Serialization Block");
-                        }
-
-                        inline for (.{ "is", "serialize" }) |func| {
-                            if (!std.meta.trait.hasFunctions(sb, .{func})) {
-                                concepts.err(concept, "missing `" ++ func ++ "` function in Serialization Block");
-                            }
-                        }
-                    },
-                    else => concepts.err(concept, "found non-namespace Serialization Block"),
-                }
+            // Check each SB in the ST.
+            for (std.meta.fields(T)) |field| {
+                @"getty.ser.sbt"(@field(sbt, field.name));
             }
         } else {
-            if (info != .Struct or info.Struct.is_tuple) {
-                concepts.err(concept, "found non-namespace Serialization Block");
+            // Check SB is a namespace.
+            if (@TypeOf(T) != type or info != .Struct or info.Struct.is_tuple) {
+                concepts.err(concept, "serialization block is not a namespace");
             }
 
+            // Check number of fields.
             if (info.Struct.fields.len != 0) {
-                concepts.err(concept, "found field in Serialization Block");
+                concepts.err(concept, "serialization block contains fields");
             }
 
-            inline for (.{ "is", "serialize" }) |func| {
-                if (!std.meta.trait.hasFunctions(T, .{func})) {
-                    concepts.err(concept, "missing `" ++ func ++ "` function in Serialization Block");
+            // Check number of declarations.
+            var num_decls = 0;
+            for (info.Struct.decls) |decl| {
+                if (decl.is_pub) {
+                    num_decls += 1;
+                }
+            }
+            if (num_decls != 2) {
+                concepts.err(concept, "serialization block contains an unexpected number of declarations");
+            }
+
+            // Check functions.
+            //
+            // We've already checked that there are only two declarations, so
+            // we don't need to check that only `serialize` or `attributes` is
+            // declared. Checking that either one of them exists is good enough
+            // as the other declaration must be `is`.
+            if (!std.meta.trait.hasFunctions(T, .{"is"})) {
+                concepts.err(concept, "serialization block missing `is` function");
+            }
+
+            if (!std.meta.trait.hasFunctions(T, .{"serialize"}) and !@hasDecl(T, "attributes")) {
+                concepts.err(concept, "serialization block must contain a `serialize` function or `attributes` declaration");
+            }
+
+            if (@hasDecl(T, "attributes")) {
+                const attr_info = @typeInfo(@TypeOf(@field(T, "attributes")));
+                if (attr_info != .Struct or !attr_info.Struct.is_tuple) {
+                    concepts.err(concept, "unexpected type for `attributes` declaration");
                 }
             }
         }
--- src/ser/concepts/traits/sbt.zig
+++ src/ser/concepts/traits/sbt.zig
@@ -1,63 +1,61 @@
 const std = @import("std");
 
 pub fn is_sbt(comptime sbt: anytype) bool {
-    const T = if (@TypeOf(sbt) == type) sbt else @TypeOf(sbt);
-    const info = @typeInfo(T);
-
     comptime {
+        const T = if (@TypeOf(sbt) == type) sbt else @TypeOf(sbt);
+        const info = @typeInfo(T);
+
         if (info == .Struct and info.Struct.is_tuple) {
-            // The SBT is a tuple.
-
-            inline for (std.meta.fields(T)) |field| {
-                const sb = @field(sbt, field.name);
-
-                if (@TypeOf(sb) != type) {
-                    // The SBT contains unexpected values (i.e., not types).
+            // Check each SB in the ST.
+            for (std.meta.fields(T)) |field| {
+                if (!is_sbt(@field(sbt, field.name))) {
                     return false;
-                }
-
-                switch (@typeInfo(sb)) {
-                    .Struct => |sb_info| {
-                        if (sb_info.is_tuple) {
-                            // The SBT contains structs, but they are tuples.
-                            return false;
-                        }
-
-                        if (sb_info.fields.len != 0) {
-                            // The SBT contains structs, but they are not namespaces.
-                            return false;
-                        }
-
-                        inline for (.{ "is", "serialize" }) |func| {
-                            if (!std.meta.trait.hasFunctions(sb, .{func})) {
-                                // The SBT contains structs, but they do not have the correct functions.
-                                return false;
-                            }
-                        }
-                    },
-                    else => return false, // The SBT does not contain structs.
                 }
             }
         } else {
-            // The SBT is not a tuple.
-
-            if (info != .Struct or info.Struct.is_tuple) {
-                // The SBT is not a struct.
+            // Check SB is a namespace.
+            if (@TypeOf(T) != type or info != .Struct or info.Struct.is_tuple) {
                 return false;
             }
 
+            // Check number of fields.
             if (info.Struct.fields.len != 0) {
-                // The SBT is not a struct namespace.
                 return false;
             }
 
-            inline for (.{ "is", "serialize" }) |func| {
-                if (!std.meta.trait.hasFunctions(T, .{func})) {
-                    // The SBT does not have the correct functions.
+            // Check number of declarations.
+            var num_decls = 0;
+            for (info.Struct.decls) |decl| {
+                if (decl.is_pub) {
+                    num_decls += 1;
+                }
+            }
+            if (num_decls != 2) {
+                return false;
+            }
+
+            // Check functions.
+            //
+            // We've already checked that there are only two declarations, so
+            // we don't need to check that only `serialize` or `attributes` is
+            // declared. Checking that either one of them exists is good enough
+            // as the other declaration must be `is`.
+            if (!std.meta.trait.hasFunctions(T, .{"is"})) {
+                return false;
+            }
+
+            if (!std.meta.trait.hasFunctions(T, .{"serialize"}) and !@hasDecl(T, "attributes")) {
+                return false;
+            }
+
+            if (@hasDecl(T, "attributes")) {
+                const attr_info = @typeInfo(@TypeOf(@field(T, "attributes")));
+                if (attr_info != .Struct or !attr_info.Struct.is_tuple) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
