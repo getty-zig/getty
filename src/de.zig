93318983c2bab7fd8f00c75466fd8aa46f04c247
Jason Phan <jphan@digitalocean.com>
2022-11-07 12:54:22 -0700

    e: Add deserialize test for std.TailQueue

--- src/de.zig
+++ src/de.zig
@@ -1039,6 +1039,95 @@ test "deserialize - struct" {
     });
 }
 
+test "deserialize - tail queue" {
+    {
+        var expected = std.TailQueue(i32){};
+
+        try t(expected, &[_]Token{
+            .{ .Seq = .{ .len = 0 } },
+            .{ .SeqEnd = {} },
+        });
+    }
+
+    {
+        var expected = std.TailQueue(i32){};
+        var one = @TypeOf(expected).Node{ .data = 1 };
+        var two = @TypeOf(expected).Node{ .data = 2 };
+        var three = @TypeOf(expected).Node{ .data = 3 };
+
+        expected.append(&one);
+        expected.append(&two);
+        expected.append(&three);
+
+        try t(expected, &[_]Token{
+            .{ .Seq = .{ .len = 3 } },
+            .{ .I32 = 1 },
+            .{ .I32 = 2 },
+            .{ .I32 = 3 },
+            .{ .SeqEnd = {} },
+        });
+    }
+
+    {
+        const Child = std.TailQueue(i32);
+        const Parent = std.TailQueue(Child);
+
+        var expected = Parent{};
+        var a = Child{};
+        var b = Child{};
+        var c = Child{};
+
+        var one = Child.Node{ .data = 1 };
+        var two = Child.Node{ .data = 2 };
+        var three = Child.Node{ .data = 3 };
+        b.append(&one);
+        c.append(&two);
+        c.append(&three);
+
+        expected.append(&Parent.Node{ .data = a });
+        expected.append(&Parent.Node{ .data = b });
+        expected.append(&Parent.Node{ .data = c });
+
+        const tokens = &[_]Token{
+            .{ .Seq = .{ .len = 3 } },
+            .{ .Seq = .{ .len = 0 } },
+            .{ .SeqEnd = {} },
+            .{ .Seq = .{ .len = 1 } },
+            .{ .I32 = 1 },
+            .{ .SeqEnd = {} },
+            .{ .Seq = .{ .len = 2 } },
+            .{ .I32 = 2 },
+            .{ .I32 = 3 },
+            .{ .SeqEnd = {} },
+            .{ .SeqEnd = {} },
+        };
+
+        // Test manually since the `t` function cannot recursively test
+        // user-defined containers containers without ugly hacks.
+        var d = TestDeserializer.init(tokens);
+        var v = deserialize(test_allocator, Parent, d.deserializer()) catch return error.TestUnexpectedError;
+        defer de.free(test_allocator, v);
+
+        try expectEqual(expected.len, v.len);
+        var iterator = expected.first;
+        while (iterator) |node| : (iterator = node.next) {
+            var got_node = v.popFirst();
+            try expect(got_node != null);
+            defer test_allocator.destroy(got_node.?);
+
+            try expectEqual(node.data.len, got_node.?.data.len);
+            var inner_iterator = node.data.first;
+            while (inner_iterator) |inner_node| : (inner_iterator = inner_node.next) {
+                var got_inner_node = got_node.?.data.popFirst();
+                try expect(got_inner_node != null);
+                defer test_allocator.destroy(got_inner_node.?);
+
+                try expectEqual(inner_node.data, got_inner_node.?.data);
+            }
+        }
+    }
+}
+
 test "deserialize - tuple" {
     try t(std.meta.Tuple(&[_]type{ i32, u32 }){ 1, 2 }, &[_]Token{
         .{ .Seq = .{ .len = 2 } },
@@ -1162,6 +1251,17 @@ fn t(expected: anytype, tokens: []const Token) !void {
                 try expectEqualSlices(std.meta.Child(T.Slice), expected.items, v.items);
             } else if (comptime std.mem.startsWith(u8, @typeName(T), "linked_list.SinglyLinkedList")) {
                 try expectEqual(expected.len(), v.len());
+                var iterator = expected.first;
+
+                while (iterator) |node| : (iterator = node.next) {
+                    var got_node = v.popFirst();
+                    try expect(got_node != null);
+                    defer test_allocator.destroy(got_node.?);
+
+                    try expectEqual(node.data, got_node.?.data);
+                }
+            } else if (comptime std.mem.startsWith(u8, @typeName(T), "linked_list.TailQueue")) {
+                try expectEqual(expected.len, v.len);
                 var iterator = expected.first;
 
                 while (iterator) |node| : (iterator = node.next) {
