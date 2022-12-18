fcddd29e4efa26abc5d27ef946dc875154c1b96a
Jason Phan <jason@ket.so>
2022-12-17 20:36:05 -0600

    ser: Fixes for type-defined blocks
    Type-defined serialization tuples are no longer a thing. Tuples are for
    grouping serialization blocks that apply to different types. However, a
    type-defined block only ever applies to the type that defined it, so it
    makes no sense for a type-defined tuple to exist.
    
    Also, type-defined serialization blocks no longer require an is
    function. As mentioned earlier, they always apply to the type that
    defined them, so there's no need for such a function.
    
    Separate is_tsb and has_sb functions have been added for type-defined
    serialization blocks.

--- src/ser/traits/sbt.zig
+++ src/ser/traits/sbt.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-/// Checks to see if a type `T` contains a serialization block or tuple.
-pub fn has_sbt(
-    /// The type to check.
+/// Checks to see if a type `T` contains a type-defined serialization block.
+pub fn has_sb(
+    /// A type containing a serialization block.
     comptime T: type,
 ) bool {
     comptime {
-        return std.meta.trait.isContainer(T) and @hasDecl(T, "getty.sbt") and is_sbt(T.@"getty.sbt");
+        return std.meta.trait.isContainer(T) and @hasDecl(T, "getty.sb") and is_tsb(T.@"getty.sb");
     }
 }
 
@@ -90,7 +90,7 @@ pub fn is_sbt(
     }
 }
 
-test "SB" {
+test "is_sbt (SB)" {
     // Not a type.
     try std.testing.expect(!is_sbt(1));
     try std.testing.expect(!is_sbt("foo"));
@@ -170,7 +170,7 @@ test "SB" {
     }));
 }
 
-test "ST" {
+test "is_sbt (ST)" {
     // Not a type.
     try std.testing.expect(!is_sbt(.{1}));
     try std.testing.expect(!is_sbt(.{"foo"}));
@@ -250,4 +250,150 @@ test "ST" {
 
         pub const attributes = .{ .x = 1, .y = 2 };
     }}));
+}
+
+/// Validates a type-defined serialization block.
+pub fn is_tsb(
+    /// A type-defined serialization block.
+    comptime sb: anytype,
+) bool {
+    comptime {
+        const SB = @TypeOf(sb);
+
+        if (SB != type) {
+            return false;
+        }
+
+        const info = @typeInfo(sb);
+
+        // Check SB is a namespace.
+        if (info != .Struct or info.Struct.is_tuple) {
+            return false;
+        }
+
+        // Check number of fields.
+        if (info.Struct.fields.len != 0) {
+            return false;
+        }
+
+        // Check number of declarations.
+        var num_decls = 0;
+        for (info.Struct.decls) |decl| {
+            if (decl.is_pub) {
+                num_decls += 1;
+            }
+        }
+        if (num_decls != 1) {
+            return false;
+        }
+
+        // Check declarations.
+        if (!std.meta.trait.hasFunctions(sb, .{"serialize"}) and !@hasDecl(sb, "attributes")) {
+            return false;
+        }
+
+        // These are just some preliminary attribute checks. The real
+        // checks are done just before Getty serializes the value.
+        if (@hasDecl(sb, "attributes")) {
+            const attr_info = @typeInfo(@TypeOf(sb.attributes));
+
+            // Check that the attributes declaration is a struct (or an empty tuple).
+            if (attr_info != .Struct or (attr_info.Struct.is_tuple and sb.attributes.len != 0)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+test "is_tsb" {
+    // Not a type.
+    try std.testing.expect(!is_tsb(1));
+    try std.testing.expect(!is_tsb("foo"));
+    try std.testing.expect(!is_tsb(.{ 1, 2, 3 }));
+    try std.testing.expect(!is_tsb(.{ .x = 1, .serialize = 2 }));
+
+    // Not a POD struct type.
+    try std.testing.expect(!is_tsb(i32));
+    try std.testing.expect(!is_tsb([]u8));
+    try std.testing.expect(!is_tsb(*struct {}));
+    try std.testing.expect(!is_tsb(std.meta.Tuple(&.{ i32, i32 })));
+
+    // Non-zero number of fields.
+    try std.testing.expect(!is_tsb(struct { x: i32 }));
+    try std.testing.expect(!is_tsb(struct { x: i32, y: i32 }));
+
+    // Incorrect number of declarations.
+    try std.testing.expect(!is_tsb(struct {
+        pub const x: i32 = 0;
+    }));
+
+    try std.testing.expect(!is_tsb(struct {
+        pub fn foo() void {
+            unreachable;
+        }
+    }));
+
+    try std.testing.expect(!is_tsb(struct {
+        pub fn is() void {
+            unreachable;
+        }
+
+        pub fn serialize() void {
+            unreachable;
+        }
+
+        pub const attributes = .{};
+    }));
+
+    try std.testing.expect(!is_tsb(struct {
+        pub fn is() void {
+            unreachable;
+        }
+
+        pub fn serialize() void {
+            unreachable;
+        }
+    }));
+
+    try std.testing.expect(!is_tsb(struct {
+        pub fn is() void {
+            unreachable;
+        }
+
+        pub const attributes = .{};
+    }));
+
+    // Incorrect declarations.
+    try std.testing.expect(!is_tsb(struct {
+        pub fn foo() void {
+            unreachable;
+        }
+
+        pub fn bar() void {
+            unreachable;
+        }
+    }));
+
+    try std.testing.expect(!is_tsb(struct {
+        pub fn is() void {
+            unreachable;
+        }
+    }));
+
+    // Success
+    try std.testing.expect(is_tsb(struct {
+        pub fn serialize() void {
+            unreachable;
+        }
+    }));
+
+    try std.testing.expect(is_tsb(struct {
+        pub const attributes = .{};
+    }));
+
+    try std.testing.expect(is_tsb(struct {
+        pub const attributes = .{ .x = 1, .y = 2 };
+    }));
 }
