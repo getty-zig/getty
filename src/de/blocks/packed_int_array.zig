47d271020e419b6deccc0d3aceb0d4c8ab4f0c31
Jason Phan <jason@ket.so>
2022-12-30 10:38:03 -0600

    de: Add support for std.PackedIntArray

---
+++ src/de/blocks/packed_int_array.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+const PackedIntArrayVisitor = @import("../impls/visitor/packed_int_array.zig").Visitor;
+
+/// Specifies all types that can be deserialized by this block.
+pub fn is(
+    /// The type being deserialized into.
+    comptime T: type,
+) bool {
+    return comptime std.mem.startsWith(u8, @typeName(T), "packed_int_array.PackedIntArrayEndian");
+}
+
+/// Specifies the deserialization process for types relevant to this block.
+pub fn deserialize(
+    /// An optional memory allocator.
+    allocator: ?std.mem.Allocator,
+    /// The type being deserialized into.
+    comptime T: type,
+    /// A `getty.Deserializer` interface value.
+    deserializer: anytype,
+    /// A `getty.de.Visitor` interface value.
+    visitor: anytype,
+) !@TypeOf(visitor).Value {
+    _ = T;
+
+    return try deserializer.deserializeSeq(allocator, visitor);
+}
+
+/// Returns a type that implements `getty.de.Visitor`.
+pub fn Visitor(
+    /// The type being deserialized into.
+    comptime T: type,
+) type {
+    return PackedIntArrayVisitor(T);
+}
---
+++ src/de/impls/visitor/packed_int_array.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+
+const de = @import("../../../de.zig").de;
+
+pub fn Visitor(comptime PackedIntArray: type) type {
+    return struct {
+        const Self = @This();
+
+        pub usingnamespace de.Visitor(
+            Self,
+            Value,
+            .{
+                .visitSeq = visitSeq,
+            },
+        );
+
+        const Value = PackedIntArray;
+
+        fn visitSeq(_: Self, allocator: ?std.mem.Allocator, comptime Deserializer: type, seq: anytype) Deserializer.Error!Value {
+            var array = Value.initAllTo(0);
+
+            if (array.len == 0) {
+                return array;
+            }
+
+            var i: usize = 0;
+            while (i < array.len) : (i += 1) {
+                if (try seq.nextElement(allocator, Value.Child)) |value| {
+                    array.set(i, value);
+                } else {
+                    // End of sequence was reached early.
+                    return error.InvalidLength;
+                }
+            }
+
+            // Expected end of sequence, but found an element.
+            if ((try seq.nextElement(allocator, de.Ignored)) != null) {
+                return error.InvalidLength;
+            }
+
+            return array;
+        }
+    };
+}
