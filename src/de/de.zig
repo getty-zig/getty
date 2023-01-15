d172cdc0b9b470d8eef9c24e8bc5eb047e5b59ce
Jason Phan <jason@ket.so>
2023-01-15 15:01:33 -0600

    de: Add getty.deserialize tests

--- src/de/de.zig
+++ src/de/de.zig
@@ -348,6 +348,314 @@ pub fn deserialize(
     return try db.deserialize(allocator, T, deserializer, v.visitor());
 }
 
+fn PointVisitor(comptime Value: type) type {
+    return struct {
+        const Self = @This();
+
+        pub usingnamespace de.Visitor(
+            Self,
+            Value,
+            .{ .visitSeq = visitSeq },
+        );
+
+        fn visitSeq(
+            _: Self,
+            allocator: ?std.mem.Allocator,
+            comptime De: type,
+            seq: anytype,
+        ) De.Error!Value {
+            var point: Value = undefined;
+
+            inline for (std.meta.fields(Value)) |field| {
+                if (try seq.nextElement(allocator, i32)) |elem| {
+                    @field(point, field.name) = elem;
+                } else {
+                    return error.InvalidLength;
+                }
+            }
+
+            if ((try seq.nextElement(allocator, de.Ignored)) != null) {
+                return error.InvalidLength;
+            }
+
+            return point;
+        }
+    };
+}
+
+test "deserialize - success, normal" {
+    const Point = struct {
+        x: i32,
+        y: i32,
+    };
+
+    const block = struct {
+        pub fn is(comptime T: type) bool {
+            return T == Point;
+        }
+
+        pub fn deserialize(a: ?std.mem.Allocator, comptime _: type, d: anytype, v: anytype) @TypeOf(d).Error!@TypeOf(v).Value {
+            return try d.deserializeSeq(a, v);
+        }
+
+        pub const Visitor = PointVisitor;
+    };
+
+    const PointCustom = struct {
+        x: i32,
+        y: i32,
+
+        const Self = @This();
+
+        pub const @"getty.db" = struct {
+            pub fn deserialize(a: ?std.mem.Allocator, comptime _: type, d: anytype, v: anytype) @TypeOf(d).Error!@TypeOf(v).Value {
+                return try d.deserializeSeq(a, v);
+            }
+
+            pub const Visitor = PointVisitor;
+        };
+    };
+
+    const expected = Point{ .x = 1, .y = 2 };
+    const expected_custom = PointCustom{ .x = 1, .y = 2 };
+
+    // Default DB
+    {
+        var d = t.de.DefaultDeserializer.init(&.{
+            .{ .Struct = .{ .name = @typeName(Point), .len = 2 } },
+            .{ .String = "x" },
+            .{ .I32 = 1 },
+            .{ .String = "y" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, Point, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // User DB
+    {
+        var d = t.de.Deserializer(block, null).init(&.{
+            .{ .Seq = .{ .len = 2 } },
+            .{ .I32 = 1 },
+            .{ .I32 = 2 },
+            .{ .SeqEnd = {} },
+        });
+
+        const got = deserialize(null, Point, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // Deserializer DB
+    {
+        var d = t.de.Deserializer(null, block).init(&.{
+            .{ .Seq = .{ .len = 2 } },
+            .{ .I32 = 1 },
+            .{ .I32 = 2 },
+            .{ .SeqEnd = {} },
+        });
+
+        const got = deserialize(null, Point, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // Type DB
+    {
+        var d = t.de.DefaultDeserializer.init(&.{
+            .{ .Seq = .{ .len = 2 } },
+            .{ .I32 = 1 },
+            .{ .I32 = 2 },
+            .{ .SeqEnd = {} },
+        });
+
+        const got = deserialize(null, PointCustom, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected_custom, got);
+    }
+}
+
+test "deserialize - success, attributes" {
+    const attrs = .{
+        .x = .{ .rename = "FOO" },
+        .y = .{ .rename = "BAR" },
+    };
+
+    const Point = struct {
+        x: i32,
+        y: i32,
+    };
+    const PointCustom = struct {
+        x: i32,
+        y: i32,
+
+        pub const @"getty.db" = struct {
+            pub const attributes = attrs;
+        };
+    };
+
+    const block = struct {
+        pub fn is(comptime T: type) bool {
+            return T == Point;
+        }
+
+        pub const attributes = attrs;
+    };
+
+    const expected = Point{ .x = 1, .y = 2 };
+    const expected_custom = PointCustom{ .x = 1, .y = 2 };
+
+    // User DB
+    {
+        var d = t.de.Deserializer(block, null).init(&.{
+            .{ .Struct = .{ .name = @typeName(Point), .len = 2 } },
+            .{ .String = "FOO" },
+            .{ .I32 = 1 },
+            .{ .String = "BAR" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, Point, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // Deserializer DB
+    {
+        var d = t.de.Deserializer(null, block).init(&.{
+            .{ .Struct = .{ .name = @typeName(Point), .len = 2 } },
+            .{ .String = "FOO" },
+            .{ .I32 = 1 },
+            .{ .String = "BAR" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, Point, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // Type DB
+    {
+        var d = t.de.DefaultDeserializer.init(&.{
+            .{ .Struct = .{ .name = @typeName(PointCustom), .len = 2 } },
+            .{ .String = "FOO" },
+            .{ .I32 = 1 },
+            .{ .String = "BAR" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, PointCustom, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected_custom, got);
+    }
+}
+
+test "deserialize - priority" {
+    const attrs = .{
+        .x = .{ .rename = "FOO" },
+        .y = .{ .rename = "BAR" },
+    };
+    const invalid_attrs = .{
+        .foo = .{ .bar = "TESTING" },
+    };
+
+    const Point = struct {
+        x: i32,
+        y: i32,
+    };
+    const PointCustom = struct {
+        x: i32,
+        y: i32,
+
+        pub const @"getty.db" = struct {
+            pub const attributes = attrs;
+        };
+    };
+    const PointInvalidCustom = struct {
+        x: i32,
+        y: i32,
+
+        pub const @"getty.db" = struct {
+            pub const attributes = invalid_attrs;
+        };
+    };
+
+    // Deserializer DB > Default DB
+    {
+        const expected = Point{ .x = 1, .y = 2 };
+
+        const deserializer_block = struct {
+            pub fn is(comptime T: type) bool {
+                return T == Point;
+            }
+
+            pub const attributes = attrs;
+        };
+
+        var d = t.de.Deserializer(null, deserializer_block).init(&.{
+            .{ .Struct = .{ .name = @typeName(Point), .len = 2 } },
+            .{ .String = "FOO" },
+            .{ .I32 = 1 },
+            .{ .String = "BAR" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, Point, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // Type DB > Deserializer DB
+    {
+        const expected = PointCustom{ .x = 1, .y = 2 };
+
+        const deserializer_block = struct {
+            pub fn is(comptime T: type) bool {
+                return T == PointCustom;
+            }
+
+            pub const attributes = invalid_attrs;
+        };
+
+        var d = t.de.Deserializer(null, deserializer_block).init(&.{
+            .{ .Struct = .{ .name = @typeName(PointCustom), .len = 2 } },
+            .{ .String = "FOO" },
+            .{ .I32 = 1 },
+            .{ .String = "BAR" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, PointCustom, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+
+    // User DB > Type DB
+    {
+        const expected = PointInvalidCustom{ .x = 1, .y = 2 };
+
+        const user_block = struct {
+            pub fn is(comptime T: type) bool {
+                return T == PointInvalidCustom;
+            }
+
+            pub const attributes = attrs;
+        };
+
+        var d = t.de.Deserializer(user_block, null).init(&.{
+            .{ .Struct = .{ .name = @typeName(PointInvalidCustom), .len = 2 } },
+            .{ .String = "FOO" },
+            .{ .I32 = 1 },
+            .{ .String = "BAR" },
+            .{ .I32 = 2 },
+            .{ .StructEnd = {} },
+        });
+
+        const got = deserialize(null, PointInvalidCustom, d.deserializer()) catch return error.UnexpectedTestError;
+        try expectEqual(expected, got);
+    }
+}
+
 test "getAttributes - fail" {
     const De = t.de.DefaultDeserializer.@"getty.Deserializer";
 
@@ -412,7 +720,7 @@ test "getAttributes - success" {
     {
         const De = t.de.DefaultDeserializer.@"getty.Deserializer";
 
-        const PointWithAttrs = struct {
+        const PointCustom = struct {
             x: i32,
             y: i32,
 
@@ -421,7 +729,7 @@ test "getAttributes - success" {
             };
         };
 
-        try expectEqual(expected, de.getAttributes(PointWithAttrs, De));
+        try expectEqual(expected, de.getAttributes(PointCustom, De));
     }
 }
 
