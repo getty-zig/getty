e4a08fdb0374cf2e6e424104b477ed0cfb39e965
Jason Phan <jason@ket.so>
2023-01-16 22:40:34 -0600

    de: Add skip & rename attribute support for unions

--- src/de/impls/visitor/union.zig
+++ src/de/impls/visitor/union.zig
@@ -15,12 +15,37 @@ pub fn Visitor(comptime Union: type) type {
         const Value = Union;
 
         fn visitUnion(_: Self, allocator: ?std.mem.Allocator, comptime Deserializer: type, ua: anytype, va: anytype) Deserializer.Error!Value {
+            const attributes = comptime de.getAttributes(Value, Deserializer);
+
             var variant = try ua.variant(allocator, []const u8);
 
             inline for (std.meta.fields(Value)) |f| {
-                if (std.mem.eql(u8, f.name, variant)) {
-                    const alloc = if (f.type == void) null else allocator;
-                    return @unionInit(Value, f.name, try va.payload(alloc, f.type));
+                comptime var name = f.name;
+
+                // Process "rename" attribute.
+                if (attributes) |attrs| {
+                    if (@hasField(@TypeOf(attrs), f.name)) {
+                        const attr = @field(attrs, f.name);
+
+                        if (@hasField(@TypeOf(attr), "rename")) {
+                            name = attr.rename;
+                        }
+                    }
+                }
+
+                if (std.mem.eql(u8, name, variant)) {
+                    // Process "skip" attribute.
+                    if (attributes) |attrs| {
+                        if (@hasField(@TypeOf(attrs), f.name)) {
+                            const attr = @field(attrs, f.name);
+
+                            if (@hasField(@TypeOf(attr), "skip") and attr.skip) {
+                                return error.UnknownVariant;
+                            }
+                        }
+                    }
+
+                    return @unionInit(Value, f.name, try va.payload(allocator, f.type));
                 }
             }
 
