a417f6a504463f0d5d296b2dfa49c2eeb0028069
Jason Phan <jason@ket.so>
2023-01-29 14:21:29 -0600

    ser, de: Reorder blocks
    Primitives have been moved ahead of std types in order to (probably?) speed
    up compile-times.

--- src/ser/blocks.zig
+++ src/ser/blocks.zig
@@ -1,44 +1,4 @@
 ////////////////////////////////////////////////////////////////////////
-// Standard Library
-////////////////////////////////////////////////////////////////////////
-
-/// Serialization block for `std.mem.Allocator` values.
-pub const Allocator = @import("blocks/allocator.zig");
-
-/// Serialization block for `std.ArrayList` values.
-pub const ArrayList = @import("blocks/array_list.zig");
-
-/// Serialization block for `std.BoundedArray` values.
-pub const BoundedArray = @import("blocks/bounded_array.zig");
-
-/// Serialization block for `std.BufMap` values.
-pub const BufMap = @import("blocks/buf_map.zig");
-
-/// Serialization block for `std.HashMap` values.
-pub const HashMap = @import("blocks/hash_map.zig");
-
-/// Serialization block for `std.SinglyLinkedList` values.
-pub const LinkedList = @import("blocks/linked_list.zig");
-
-/// Serialization block for `std.net.Address` values.
-pub const NetAddress = @import("blocks/net_address.zig");
-
-/// Serialization block for `std.PackedIntArray` and `std.PackedIntSlice` values.
-pub const PackedInt = @import("blocks/packed_int.zig");
-
-/// Serialization block for `std.TailQueue`.
-pub const TailQueue = @import("blocks/tail_queue.zig");
-
-////////////////////////////////////////////////////////////////////////////
-// Struct
-//
-// IMPORTANT: All user-defined types must be listed BEFORE this section.
-////////////////////////////////////////////////////////////////////////////
-
-/// Serialization block for `struct` values.
-pub const Struct = @import("blocks/struct.zig");
-
-////////////////////////////////////////////////////////////////////////
 // Primitives
 ////////////////////////////////////////////////////////////////////////
 
@@ -78,11 +38,53 @@ pub const String = @import("blocks/string.zig");
 /// Serialization block for tuple values.
 pub const Tuple = @import("blocks/tuple.zig");
 
-/// Serialization block for `union` values.
-pub const Union = @import("blocks/union.zig");
-
 /// Serialization block for vector values.
 pub const Vector = @import("blocks/vector.zig");
 
 /// Serialization block for `void` values.
 pub const Void = @import("blocks/void.zig");
+
+////////////////////////////////////////////////////////////////////////
+// Standard Library
+////////////////////////////////////////////////////////////////////////
+
+/// Serialization block for `std.mem.Allocator` values.
+pub const Allocator = @import("blocks/allocator.zig");
+
+/// Serialization block for `std.ArrayList` values.
+pub const ArrayList = @import("blocks/array_list.zig");
+
+/// Serialization block for `std.BoundedArray` values.
+pub const BoundedArray = @import("blocks/bounded_array.zig");
+
+/// Serialization block for `std.BufMap` values.
+pub const BufMap = @import("blocks/buf_map.zig");
+
+/// Serialization block for `std.HashMap` values.
+pub const HashMap = @import("blocks/hash_map.zig");
+
+/// Serialization block for `std.SinglyLinkedList` values.
+pub const LinkedList = @import("blocks/linked_list.zig");
+
+/// Serialization block for `std.net.Address` values.
+pub const NetAddress = @import("blocks/net_address.zig");
+
+/// Serialization block for `std.PackedIntArray` and `std.PackedIntSlice` values.
+pub const PackedInt = @import("blocks/packed_int.zig");
+
+/// Serialization block for `std.TailQueue`.
+pub const TailQueue = @import("blocks/tail_queue.zig");
+
+////////////////////////////////////////////////////////////////////////////
+// Aggregates
+//
+// IMPORTANT: All user-defined types must be listed BEFORE this section.
+//            Each type in this section has user-defined aliases that are
+//            supported by Getty (e.g., std.ArrayList is a struct).
+////////////////////////////////////////////////////////////////////////////
+
+/// Serialization block for `struct` values.
+pub const Struct = @import("blocks/struct.zig");
+
+/// Serialization block for `union` values.
+pub const Union = @import("blocks/union.zig");
