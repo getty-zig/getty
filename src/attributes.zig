f1dca3602e45346d7e81f401ece3954936b8c601
Jason Phan <jason@ket.so>
2023-02-10 16:42:25 -0600

    attrs: Add default field attributes
    This required changing InnerAttributes into a generic type so that it
    can use information about the base struct type's fields.

--- src/attributes.zig
+++ src/attributes.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-
+const comptimePrint = std.fmt.comptimePrint;
 const Type = std.builtin.Type;
 
 const ContainerAttributes = struct {
@@ -47,78 +47,6 @@ const ContainerAttributes = struct {
 
     // Use the untagged enum representation for this enum.
     //untagged: bool = false,
-};
-
-const FieldAttributes = struct {
-    // Deserialize this field from the given names *or* its type
-    // name.
-    //alias: ?[][]const u8 = null,
-
-    // If the value is not present when deserializing, use the
-    // field's default value.
-    //default: bool = false,
-
-    // If the value is not present when deserializing, call a
-    // function to get a default value.
-    //
-    // Overrides `default`.
-    //default_path: ?[]const u8 = null,
-
-    // Deserialize this field using a function that is different
-    // from the normal deserialization implementation.
-    //deserialize_with: ?[]const u8 = null,
-
-    // Flatten the contents of this field into the container it is
-    // defined in.
-    //
-    // This attribute is not compatible with the ignore_unknown_fields
-    // attribute.
-    //flatten: bool = false,
-
-    // Serialize and deserialize this field with the given name
-    // instead of its type name.
-    rename: ?[]const u8 = null,
-
-    // Skip this field during serialization and deserialziation.
-    //
-    // During deserialization, the field's default value will be
-    // used.
-    skip: bool = false,
-
-    // Call a function to determine whether to skip serializing
-    // this field.
-    //skip_ser_if: ?[]const u8 = null,
-
-    // Serialize this field using a function that is different from
-    // the normal serialization implementation.
-    //serialize_with: ?[]const u8 = null,
-
-    // Combination of serialize_with and deserialize_with.
-    //with: ?[]const u8 = null,
-};
-
-const VariantAttributes = struct {
-    // Deserialize this variant from the given names *or* its type
-    // name.
-    //alias: ?[][]const u8 = null,
-
-    // Deserialize this variant using a function that is different
-    // from the normal deserialization implementation.
-    //deserialize_with: ?[]const u8 = null,
-
-    // Serialize and deserialize this variant with the given name
-    // instead of its type name.
-    rename: ?[]const u8 = null,
-
-    // Never serialize or deserialize this variant.
-    skip: bool = false,
-
-    // Serialize this variant using a function that is different from
-    // the normal serialization implementation.
-    //serialize_with: ?[]const u8 = null,
-
-    // Combination of serialize_with and deserialize_with.
-    //with: ?[]const u8 = null,
 };
 
 pub const Case = enum {
@@ -234,62 +162,145 @@ pub const Case = enum {
 ///   }
 ///   ```
 pub fn Attributes(comptime T: type, comptime attributes: anytype) type {
-    comptime {
-        const type_name = @typeName(T);
-        const A = @TypeOf(attributes);
-        const attributes_info = @typeInfo(A);
+    const type_name = @typeName(T);
+    const A = @TypeOf(attributes);
+    const attributes_info = @typeInfo(A);
 
-        if (attributes_info != .Struct) {
-            @compileError(std.fmt.comptimePrint("expected attributes to be a struct, found `{s}`", .{@typeName(A)}));
-        }
-
-        if (attributes_info.Struct.fields.len == 0) {
-            return struct {};
-        }
-
-        const InnerAttributes = switch (@typeInfo(T)) {
-            .Struct => FieldAttributes,
-            .Union => VariantAttributes,
-            else => @compileError(std.fmt.comptimePrint("expected attributes to be defined in a struct or union, found `{s}`", .{type_name})),
-        };
-        const container = &ContainerAttributes{};
-        const inner = &InnerAttributes{};
-
-        var fields: [attributes_info.Struct.fields.len]Type.StructField = undefined;
-
-        inline for (attributes_info.Struct.fields) |field, i| {
-            if (@hasField(T, field.name)) {
-                // If a field in the passed-in attribute map matches a field in the
-                // passed-in value, add the inner attribute to the fields array.
-                fields[i] = .{
-                    .name = field.name,
-                    .type = InnerAttributes,
-                    .default_value = inner,
-                    .is_comptime = false,
-                    .alignment = 4,
-                };
-            } else if (std.mem.eql(u8, field.name, "Container")) {
-                // If a field in the passed-in attribute map matches the word
-                // "Container", add the container attribute to the fields array.
-                fields[i] = .{
-                    .name = "Container",
-                    .type = ContainerAttributes,
-                    .default_value = container,
-                    .is_comptime = false,
-                    .alignment = 4,
-                };
-            } else {
-                @compileError(std.fmt.comptimePrint("nonexistent field/variant `{s}` in type `{s}`", .{ field.name, type_name }));
-            }
-        }
-
-        return @Type(Type{
-            .Struct = .{
-                .layout = .Auto,
-                .fields = &fields,
-                .decls = &[_]Type.Declaration{},
-                .is_tuple = false,
-            },
-        });
+    // Check that attributes is a struct.
+    if (attributes_info != .Struct) {
+        @compileError(comptimePrint("expected attributes to be a struct, found `{s}`", .{@typeName(A)}));
     }
+
+    // If attributes contains no fields, return an empty attribute map.
+    if (attributes_info.Struct.fields.len == 0) {
+        return struct {};
+    }
+
+    const container = &ContainerAttributes{};
+
+    var fields: [attributes_info.Struct.fields.len]Type.StructField = undefined;
+
+    inline for (attributes_info.Struct.fields) |field, i| {
+        if (@hasField(T, field.name)) {
+            for (std.meta.fields(T)) |f| {
+                if (std.mem.eql(u8, field.name, f.name)) {
+                    const Attrs = InnerAttributes(T, f.type);
+                    const attrs = &Attrs{};
+
+                    // If a field in the passed-in attribute map matches a field in the
+                    // passed-in value, add the inner attribute to the fields array.
+                    fields[i] = .{
+                        .name = field.name,
+                        .type = Attrs,
+                        .default_value = attrs,
+                        .is_comptime = false,
+                        .alignment = 4,
+                    };
+
+                    break;
+                }
+            }
+        } else if (std.mem.eql(u8, field.name, "Container")) {
+            // If a field in the passed-in attribute map matches the word
+            // "Container", add the container attribute to the fields array.
+            fields[i] = .{
+                .name = "Container",
+                .type = ContainerAttributes,
+                .default_value = container,
+                .is_comptime = false,
+                .alignment = 4,
+            };
+        } else {
+            @compileError(comptimePrint("nonexistent field/variant `{s}` in type `{s}`", .{ field.name, type_name }));
+        }
+    }
+
+    return @Type(Type{
+        .Struct = .{
+            .layout = .Auto,
+            .fields = &fields,
+            .decls = &[_]Type.Declaration{},
+            .is_tuple = false,
+        },
+    });
+}
+
+fn InnerAttributes(comptime T: type, comptime Field: type) type {
+    const FieldAttributes = struct {
+        // Deserialize this field from the given names *or* its type
+        // name.
+        //alias: ?[][]const u8 = null,
+
+        // If the value is not present when deserializing, use the
+        // field's default value.
+        default: ?Field = null,
+
+        // If the value is not present when deserializing, call a
+        // function to get a default value.
+        //
+        // Overrides `default`.
+        //default_path: ?[]const u8 = null,
+
+        // Deserialize this field using a function that is different
+        // from the normal deserialization implementation.
+        //deserialize_with: ?[]const u8 = null,
+
+        // Flatten the contents of this field into the container it is
+        // defined in.
+        //
+        // This attribute is not compatible with the ignore_unknown_fields
+        // attribute.
+        //flatten: bool = false,
+
+        // Serialize and deserialize this field with the given name
+        // instead of its type name.
+        rename: ?[]const u8 = null,
+
+        // Skip this field during serialization and deserialziation.
+        //
+        // During deserialization, the field's default value will be
+        // used.
+        skip: bool = false,
+
+        // Call a function to determine whether to skip serializing
+        // this field.
+        //skip_ser_if: ?[]const u8 = null,
+
+        // Serialize this field using a function that is different from
+        // the normal serialization implementation.
+        //serialize_with: ?[]const u8 = null,
+
+        // Combination of serialize_with and deserialize_with.
+        //with: ?[]const u8 = null,
+    };
+
+    const VariantAttributes = struct {
+        // Deserialize this variant from the given names *or* its type
+        // name.
+        //alias: ?[][]const u8 = null,
+
+        // Deserialize this variant using a function that is different
+        // from the normal deserialization implementation.
+        //deserialize_with: ?[]const u8 = null,
+
+        // Serialize and deserialize this variant with the given name
+        // instead of its type name.
+        rename: ?[]const u8 = null,
+
+        // Never serialize or deserialize this variant.
+        skip: bool = false,
+
+        // Serialize this variant using a function that is different from
+        // the normal serialization implementation.
+        //serialize_with: ?[]const u8 = null,
+
+        // Combination of serialize_with and deserialize_with.
+        //with: ?[]const u8 = null,
+    };
+
+    return switch (@typeInfo(T)) {
+        .Struct => FieldAttributes,
+        .Union => VariantAttributes,
+        else => @compileError(comptimePrint("expected attributes to be defined in a struct or union, found `{s}`", .{@typeName(T)})),
+    };
 }
