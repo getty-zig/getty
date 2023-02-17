b168060e1b2183ac5729622d5b7974e3a06782d7
Jason Phan <jason@ket.so>
2023-02-17 12:45:51 -0600

    ser: Fix ArrayBitSet serialization
    The logic for serializing unset bits after the initial serialization
    loop was incorrect in that would never serialize the last bit as 0 if
    the bit in position 1 was set.
    
    This was because the 0th bit serialization logic relied on the i
    variable, which would always be decremented down to 0 if the bit at
    position 1 was set, meaning that N - 1 bits would be serialized instead
    of N bits.
    
    To fix this, the last_pos variable was introduced to track the actual
    last position in value that is set.

--- src/ser/blocks/array_bit_set.zig
+++ src/ser/blocks/array_bit_set.zig
@@ -21,37 +21,41 @@ pub fn serialize(
 ) @TypeOf(serializer).Error!@TypeOf(serializer).Ok {
     _ = allocator;
 
-    const Value = @TypeOf(value);
-    const MaskInt = Value.MaskInt;
-    const zero = @as(MaskInt, 0);
-    const one = @as(MaskInt, 1);
-
     const cap = value.capacity();
     std.debug.assert(cap != 0); // std.IntegerBitSet would be used if cap was 0.
 
     var s = try serializer.serializeSeq(cap);
     const seq = s.seq();
 
-    // Serialize bits from N to M, where N is value's bit length and M is the
-    // last bit position in value that is set.
-    var it = value.iterator(.{ .direction = .reverse });
-    var i: usize = cap - 1;
+    const MaskInt = @TypeOf(value).MaskInt;
+    const zero = @as(MaskInt, 0);
+    const one = @as(MaskInt, 1);
 
-    while (it.next()) |pos| {
-        while (i != pos) {
-            try seq.serializeElement(zero);
+    // The position of the last set bit in value.
+    var last_pos = cap;
+
+    {
+        // Serialize bits from N to M, where N is value's bit length and M is
+        // the position of the last set bit in value.
+        var it = value.iterator(.{ .direction = .reverse });
+        var i: usize = cap - 1;
+
+        while (it.next()) |pos| {
+            while (i != pos) {
+                try seq.serializeElement(zero);
+                if (i > 0) i -= 1;
+            }
+
+            try seq.serializeElement(one);
             if (i > 0) i -= 1;
-        }
 
-        try seq.serializeElement(one);
-        if (i > 0) i -= 1;
+            last_pos = pos;
+        }
     }
 
     // Serialize remaining bits (which are all unset).
-    if (i != 0) {
-        i += 1;
-
-        while (i != 0) : (i -= 1) {
+    if (last_pos != 0) {
+        while (last_pos != 0) : (last_pos -= 1) {
             try seq.serializeElement(zero);
         }
     }
@@ -59,10 +63,15 @@ pub fn serialize(
     return try seq.end();
 }
 
-// NOTE: The token sequence's length needs to match the bitset's size, which
-// must be a bit width supported by the test serializer.
 test "serialize - std.ArrayBitSet" {
-    const size = @bitSizeOf(usize) + 1;
+    // Any size <= @bitSizeOf(usize) will result in std.IntegerBitSet being
+    // used. So, 1 is added to size to make sure that std.ArrayBitSet is used.
+    //
+    // Also, another 1 is added to size to ensure that the bitset's length is
+    // even, so that the LSB and MSB won't be the same. This lets us make sure
+    // that the last element of a sequence is being serialized as the LSB of a
+    // bitset, and not the MSB.
+    const size = @bitSizeOf(usize) + 2;
 
     // Empty
     {
@@ -70,6 +79,7 @@ test "serialize - std.ArrayBitSet" {
 
         try t.run(null, serialize, want, &.{
             .{ .Seq = .{ .len = size } },
+            .{ .U64 = 0 },
             .{ .U64 = 0 },
             .{ .U64 = 0 },
             .{ .U64 = 0 },
@@ -210,45 +220,18 @@ test "serialize - std.ArrayBitSet" {
             .{ .U64 = 1 },
             .{ .U64 = 1 },
             .{ .U64 = 1 },
+            .{ .U64 = 1 },
             .{ .SeqEnd = {} },
         });
     }
 
-    // Mixed
+    // Mixed (LSB set)
     {
         var want = std.StaticBitSet(size).initEmpty();
-        want.set(1);
-        want.set(3);
-        want.set(5);
-        want.set(7);
-        want.set(9);
-        want.set(11);
-        want.set(13);
-        want.set(15);
-        want.set(17);
-        want.set(19);
-        want.set(21);
-        want.set(23);
-        want.set(25);
-        want.set(27);
-        want.set(29);
-        want.set(31);
-        want.set(33);
-        want.set(35);
-        want.set(37);
-        want.set(39);
-        want.set(41);
-        want.set(43);
-        want.set(45);
-        want.set(47);
-        want.set(49);
-        want.set(51);
-        want.set(53);
-        want.set(55);
-        want.set(57);
-        want.set(59);
-        want.set(61);
-        want.set(63);
+        {
+            var i: usize = 0; // 0, 2, 4, ..., size - 2
+            while (i < size) : (i += 2) want.set(i);
+        }
 
         try t.run(null, serialize, want, &.{
             .{ .Seq = .{ .len = size } },
@@ -316,6 +299,88 @@ test "serialize - std.ArrayBitSet" {
             .{ .U64 = 1 },
             .{ .U64 = 0 },
             .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .SeqEnd = {} },
+        });
+    }
+
+    // Mixed (LSB unset)
+    {
+        var want = std.StaticBitSet(size).initEmpty();
+        {
+            var i: usize = 1; // 1, 3, 5, ..., size - 1
+            while (i < size) : (i += 2) want.set(i);
+        }
+
+        try t.run(null, serialize, want, &.{
+            .{ .Seq = .{ .len = size } },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
+            .{ .U64 = 1 },
+            .{ .U64 = 0 },
             .{ .SeqEnd = {} },
         });
     }
