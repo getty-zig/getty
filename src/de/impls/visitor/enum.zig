a6d9f1e8456b221bbaaa4f514257efe406cbc2a6
ion232 <66679118+ion232@users.noreply.github.com>
2023-02-27 10:57:46 +0000

    refactor: clean up enum deserialization visitor

--- src/de/impls/visitor/enum.zig
+++ src/de/impls/visitor/enum.zig
@@ -20,17 +20,16 @@ pub fn Visitor(comptime Enum: type) type {
         const Value = Enum;
 
         fn visitEnum(_: Self, _: ?std.mem.Allocator, comptime Deserializer: type, input: anytype) Deserializer.Error!Value {
-            return visitString(@tagName(input));
+            return try visitString(@tagName(input));
         }
 
         fn visitInt(_: Self, _: ?std.mem.Allocator, comptime Deserializer: type, input: anytype) Deserializer.Error!Value {
             const fields = std.meta.fields(Value);
             const attributes = comptime getAttributes(Value, Deserializer);
-            const result = std.meta.intToEnum(Value, input) catch error.InvalidValue;
-            const resulta = try result;
+            const result = std.meta.intToEnum(Value, input) catch return error.InvalidValue;
 
             inline for (fields) |field| {
-                if (std.mem.eql(u8, field.name, @tagName(resulta))) {
+                if (std.meta.isTag(result, field.name)) {
                     if (attributes) |attrs| {
                         if (@hasField(@TypeOf(attrs), field.name)) {
                             const attr = @field(attrs, field.name);
@@ -38,6 +37,7 @@ pub fn Visitor(comptime Enum: type) type {
                             if (skipped) return error.InvalidValue;
                         }
                     }
+                    break;
                 }
             }
 
