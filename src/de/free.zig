85f67d5c4fba808e392826113c05a93ebc862be8
Danh Phan <input@danh.computer>
2023-08-18 20:59:14 -0500

    de: Document allocation model
    Closes #129.

--- src/de/free.zig
+++ src/de/free.zig
@@ -7,9 +7,26 @@ const find_db = @import("find.zig").find_db;
 /// Frees `v` using an allocator `ally` according to the deserialization blocks
 /// specified in the `getty.Deserializer` type `D`.
 ///
-/// `free` is intended to be used for freeing values deserialized by Getty. `v`
-/// must be a fully deserialized value. Attempts to free partially deserialized
-/// values will result in undefined behavior.
+/// `free` is intended to be used only for freeing values fully deserialized by
+/// Getty. Freeing partially deserialized values with `free` will result in
+/// undefined behavior.
+///
+/// Getty's allocation model during deserialization is as follows:
+///
+///   - Deserialized pointer values returned by an access method (e.g.,
+///     `nextKey`) will be freed upon failure and, where applicable [1], upon
+///     success if the value's corresponding `isXAllocated` method (e.g.,
+///     `isKeyAllocated`) returns true.
+///
+///   - Deserialized pointer values not returned by an access method will
+///     always be freed upon failure and, where applicable [1], upon success.
+///
+///
+/// 1. Generally speaking, "where applicable" means whenever a deserialized
+///    value is not part of the final value returned to the end user. For
+///    example, if a heap-allocated string is visited to produce an integer,
+///    the string isn't part of the final, returned integer, and so it should
+///    be freed even upon success.
 pub fn free(
     /// A memory allocator.
     ally: std.mem.Allocator,
