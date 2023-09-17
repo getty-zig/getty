52c28847b61f782d2c93a5d7a7eebffd51beec14
Danh Phan <input@danh.computer>
2023-09-16 22:01:49 -0500

    de: Update return value for visitString in SemanticVersion
    The logic that acquires the starting/ending indices is a modified
    version of std.SemanticVersion.parse's.

--- src/de/impls/visitor/semantic_version.zig
+++ src/de/impls/visitor/semantic_version.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 
 const free = @import("../../free.zig").free;
+const Range = @import("../../interfaces/visitor.zig").Range;
 const StringLifetime = @import("../../lifetime.zig").StringLifetime;
 const VisitorInterface = @import("../../interfaces/visitor.zig").Visitor;
+const VisitStringReturn = @import("../../interfaces/visitor.zig").VisitStringReturn;
 
 const Visitor = @This();
 
@@ -22,7 +24,7 @@ fn visitString(
     comptime Deserializer: type,
     input: anytype,
     lifetime: StringLifetime,
-) Deserializer.Err!Value {
+) Deserializer.Err!VisitStringReturn(Value) {
     const heap_lt = lifetime == .heap;
 
     if (!heap_lt and ally == null) {
@@ -33,7 +35,7 @@ fn visitString(
     errdefer if (!heap_lt) free(ally.?, Deserializer, ver);
 
     if (ver.pre == null and ver.build == null) {
-        return ver;
+        return .{ .value = ver };
     }
 
     if (ver.pre) |pre| {
@@ -43,5 +45,63 @@ fn visitString(
         ver.build = if (heap_lt) build else try ally.?.dupe(u8, build);
     }
 
-    return ver;
+    if (!heap_lt) {
+        return .{ .value = ver };
+    }
+
+    // This will always be safe to unwrap since we've already checked that
+    // either a pre or build value exists within the semver value.
+    const extra_idx = std.mem.indexOfAny(u8, input, "-+").?;
+    const extra = input[extra_idx.?..input.len];
+
+    var build_start: usize = undefined;
+    var build_end: usize = undefined;
+
+    if (extra[0] == '-') {
+        // There is a pre value.
+        var pre_start: usize = undefined;
+        var pre_end: usize = undefined;
+
+        const build_idx = std.mem.indexOfScalar(u8, extra, '+');
+        pre_start = extra_idx.? + 1;
+        pre_end = if (build_idx != null) extra_idx + build_idx else extra.len;
+
+        if (build_idx) |idx| {
+            // There is a build value.
+            build_start = extra_idx.? + idx + 1;
+            build_end = input.len;
+        } else {
+            // There is no build value, so we can return early.
+            return .{
+                .value = ver,
+                .used = .{ .one = .{ .start = pre_start, .end = pre_end } },
+            };
+        }
+
+        if (ally == null) {
+            return error.MissingAllocator;
+        }
+
+        // Process pre and build values.
+        var used = used: {
+            var used = try ally.?.alloc([]const Range, 2);
+            used[0] = .{ .start = pre_start, .end = pre_end };
+            used[1] = .{ .start = build_start, .end = build_end };
+            break :used used;
+        };
+
+        return .{
+            .value = ver,
+            .used = .{ .many = used },
+        };
+    }
+
+    // There is only a build value.
+    build_start = extra_idx.? + 1;
+    build_end = input.len;
+
+    return .{
+        .value = ver,
+        .used = .{ .one = .{ .start = build_start, .end = build_end } },
+    };
 }
