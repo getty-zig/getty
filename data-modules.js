191c83ae00efe87f4221ad775c532395632edc2b
ibokuri <input@danh.computer>
2023-09-21 15:42:06 +0000

    Update docs to output generated at 3c33066

--- data-modules.js
+++ data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"getty","file":0,"main":66,"table":{"getty":0,"std":1,"builtin":2}},{"name":"std","file":0,"main":68,"table":{}},{"name":"builtin","file":0,"main":36274,"table":{}}];
+var modules =[{"name":"getty","file":0,"main":66,"table":{"getty":0,"std":1,"builtin":2}},{"name":"std","file":0,"main":68,"table":{}},{"name":"builtin","file":0,"main":36578,"table":{}}];
