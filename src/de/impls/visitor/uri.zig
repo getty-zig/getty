c46b9a164be396d3380cdb3e6955dfbfe970335d
Danh Phan <input@danh.computer>
2023-09-21 23:23:15 -0500

    de: Update URI visitor with lifetime

--- src/de/impls/visitor/uri.zig
+++ src/de/impls/visitor/uri.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+const StringLifetime = @import("../../lifetime.zig").StringLifetime;
 const VisitorInterface = @import("../../interfaces/visitor.zig").Visitor;
 
 const Visitor = @This();
@@ -14,26 +15,37 @@ pub usingnamespace VisitorInterface(
 
 const Value = std.Uri;
 
-fn visitString(_: Visitor, ally: std.mem.Allocator, comptime Deserializer: type, input: anytype) Deserializer.Err!Value {
+fn visitString(
+    _: Visitor,
+    ally: std.mem.Allocator,
+    comptime Deserializer: type,
+    input: anytype,
+    lt: StringLifetime,
+) Deserializer.Err!Value {
     var uri = std.Uri.parse(input) catch return error.InvalidValue;
 
-    uri.scheme = try ally.dupe(u8, uri.scheme);
-    uri.path = try ally.dupe(u8, uri.path);
+    switch (lt) {
+        .heap => {},
+        .stack, .owned => {
+            uri.scheme = try ally.dupe(u8, uri.scheme);
+            uri.path = try ally.dupe(u8, uri.path);
 
-    if (uri.host) |host| {
-        uri.host = try ally.dupe(u8, host);
-    }
-    if (uri.user) |user| {
-        uri.user = try ally.dupe(u8, user);
-    }
-    if (uri.password) |password| {
-        uri.password = try ally.dupe(u8, password);
-    }
-    if (uri.query) |query| {
-        uri.query = try ally.dupe(u8, query);
-    }
-    if (uri.fragment) |fragment| {
-        uri.fragment = try ally.dupe(u8, fragment);
+            if (uri.host) |host| {
+                uri.host = try ally.dupe(u8, host);
+            }
+            if (uri.user) |user| {
+                uri.user = try ally.dupe(u8, user);
+            }
+            if (uri.password) |password| {
+                uri.password = try ally.dupe(u8, password);
+            }
+            if (uri.query) |query| {
+                uri.query = try ally.dupe(u8, query);
+            }
+            if (uri.fragment) |fragment| {
+                uri.fragment = try ally.dupe(u8, fragment);
+            }
+        },
     }
 
     return uri;
