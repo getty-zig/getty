58b50184effe684c17f1401a8662c53f3f5faf0c
Danh Phan <input@danh.computer>
2023-09-21 23:28:49 -0500

    de: Update slice visitor with lifetime

--- src/de/impls/visitor/slice.zig
+++ src/de/impls/visitor/slice.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+const StringLifetime = @import("../../lifetime.zig").StringLifetime;
 const VisitorInterface = @import("../../interfaces/visitor.zig").Visitor;
 
 pub fn Visitor(comptime Slice: type) type {
@@ -33,17 +34,46 @@ pub fn Visitor(comptime Slice: type) type {
             return try list.toOwnedSlice();
         }
 
-        fn visitString(_: Self, ally: std.mem.Allocator, comptime Deserializer: type, input: anytype) Deserializer.Err!Value {
+        // input is only used directly if it the following are true:
+        //
+        //   1. input is a Heap value.
+        //   2. The sentinel values of input and Value match.
+        //   3. Either constness of input and Value match or are compatible
+        //      (i.e., Value is const and input is not const).
+        //
+        //  Otherwise, input is copied into a new slice.
+        fn visitString(
+            _: Self,
+            ally: std.mem.Allocator,
+            comptime Deserializer: type,
+            input: anytype,
+            lt: StringLifetime,
+        ) Deserializer.Err!Value {
             if (Child != u8) {
                 return error.InvalidType;
             }
 
-            const sentinel = @typeInfo(Value).Pointer.sentinel;
+            const v_info = @typeInfo(Value).Pointer;
 
-            const output = try ally.alloc(u8, input.len + @intFromBool(sentinel != null));
+            switch (lt) {
+                .heap => {
+                    const i_info = @typeInfo(@TypeOf(input)).Pointer;
+
+                    const sentinels_match = (v_info.sentinel == null) == (i_info.sentinel == null);
+                    const constness_match = v_info.is_const == i_info.is_const;
+                    const constness_compat = v_info.is_const and !i_info.is_const;
+
+                    if (sentinels_match and (constness_match or constness_compat)) {
+                        return @as(Value, input);
+                    }
+                },
+                .stack, .owned => {},
+            }
+
+            const output = try ally.alloc(u8, input.len + @intFromBool(v_info.sentinel != null));
             std.mem.copy(u8, output, input);
 
-            if (sentinel) |s| {
+            if (v_info.sentinel) |s| {
                 const sentinel_char = @as(*const u8, @ptrCast(s)).*;
                 output[input.len] = sentinel_char;
                 return output[0..input.len :sentinel_char];
