90e6c831183a063373f5480fd2a4e5f853b87fd9
ibokuri <input@danh.computer>
2023-11-21 04:26:19 +0000

    Update docs to output generated at 14c64a1

--- data-modules.js
+++ data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"getty","file":0,"main":66,"table":{"builtin":2,"std":1,"getty":0}},{"name":"std","file":0,"main":70,"table":{}},{"name":"builtin","file":0,"main":32173,"table":{}}];
+var modules =[{"name":"getty","file":0,"main":66,"table":{"getty":0,"std":1,"builtin":2}},{"name":"std","file":0,"main":70,"table":{}},{"name":"builtin","file":0,"main":32173,"table":{}}];
